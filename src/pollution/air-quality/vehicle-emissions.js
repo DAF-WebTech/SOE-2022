d40fbfcf151c1e119af804d593c013475ddf330c
David Porter <david.porter@fastmail.com>
2021-08-24 22:42:30 +0000

    Update vehicle-emissions.js

--- src/pollution/air-quality/vehicle-emissions.js
+++ src/pollution/air-quality/vehicle-emissions.js
@@ -1,23 +1,24 @@
 "use strict"
 
-
 document.addEventListener("DOMContentLoaded", function () {
 
+	// sort data
+	const data = soefinding.findingJson.data.sort(function(a, b) {
+		return b["Tonnes per annum"] - a["Tonnes per annum"]
+	})
 
 	// chart 1. pie, emissions
-	const series = soefinding.findingJson.data.map(d => d["Tonnes per annum"])
+	const series = data.map(d => d["Tonnes per annum"])
 
 	// create the vue instance for first chart, our column chart
 	const options1 = soefinding.getDefaultPieChartOptions()
 	// the pie charts uses labels, but the table vue is looking for categories
-	options1.labels = options1.categories = soefinding.findingJson.data.map(d => d["Emission type"])
-// 	options1.yaxis.title.text = "Kilometres travelled (billions)"
-// 	options1.tooltip.y = {
-// 		formatter: val => (val*1000000000).toLocaleString()
-// 	}
-// 	options1.yaxis.labels.formatter = function (val) {
-// 		return val
-// 	}
+	options1.labels = data.map(d => d["Emission type"])
+	options1.tooltip = { y: { formatter: (val, options) => {
+		const percent = options.globals.seriesPercent[options.seriesIndex][0]
+		return `${val.toLocaleString()} (${percent.toFixed(1)}%)`
+	}}}
+	options1.xaxis.categories = ["Emission type", "Tonnes per annum"] // not needed for chart, but vue uses them for table headings
 
 	soefinding.state.chart1 = {
 		options: options1,
@@ -30,11 +31,10 @@ document.addEventListener("DOMContentLoaded", function () {
 		el: "#chartContainer",
 		data: soefinding.state,
 		computed: {
-			heading1: function () { return `Proportion of annual average vehicle emissions by type in 2010` },
-
+			heading1: () => `Proportion of annual average vehicle emissions by type in 2010`,
 		},
 		methods: {
-			formatter1: function (val) { return val?.toLocaleString(undefined, { minimumFractionDigits: 2 }) ?? "" }
+			formatter1: val => val.toLocaleString() 
 		}
 	})
 })
