39e3a9fa0ae7da1612b5bfacd7062a3ee469e913
PORTER David <david.porter@daf.qld.gov.au>
2021-08-30 10:50:33 +1000

    add all charts

--- src/pollution/greenhouse-gas-omissions/total-annual-greenhouse.js
+++ src/pollution/greenhouse-gas-omissions/total-annual-greenhouse.js
@@ -2,13 +2,16 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-	const latestYear = soefinding.findingJson.meta.fields[soefinding.findingJson.meta.fields.length - 1]
+	const yearKeys = soefinding.findingJson.meta.fields.slice(2)
+	const latestYear = yearKeys[yearKeys.length - 1]
 
 	// chart 1. pie, Queensland proportion
-	const qldSectors = data.filter(d => d.State == "Queensland" && d.Sector != "All (incl. LULUCF)")
+	const qldSectors = soefinding.findingJson.data.filter(d => d.State == "Queensland" && d.Sector != "All (incl. LULUCF)")
+	qldSectors.sort(function (a, b) {
+		return b[latestYear] - a[latestYear]
+	})
 	const proportionSeries = qldSectors.map(d => d[latestYear])
 
-	// create the vue instance for first chart, our column chart
 	const options1 = soefinding.getDefaultPieChartOptions()
 	// the pie charts uses labels, but the table vue is looking for categories
 	options1.labels = qldSectors.map(d => d.Sector)
@@ -28,15 +31,100 @@ document.addEventListener("DOMContentLoaded", function () {
 		chartactive: true,
 	};
 
+	// 2. stacked columns, all states all sectors latest year
+	// 	const states = {}
+	// 	soefinding.findingJson.data.forEach(d => {
+	// 		if (d.Sector != "All" && d.Sector != "All (incl. LULUCF)") {
+	// 			if (!states[d.State])
+	// 				states[d.State] = []
+	// 			states[d.State].push(d)
+	// 		}
+	// 	})
+	const sectors = {}
+	soefinding.findingJson.data.forEach(d => {
+		if (d.Sector != "All" && d.Sector != "All (incl. LULUCF)") {
+			if (!sectors[d.Sector])
+				sectors[d.Sector] = []
+			sectors[d.Sector].push(d)
+		}
+	})
+	const sectorSeries = Object.keys(sectors).map(k => {
+		return {
+			name: k,
+			data: sectors[k].map(d => d[latestYear])
+		}
+	})
+
+	const options2 = soefinding.getDefaultBarChartOptions()
+	options2.chart.stacked = true
+	options2.xaxis.categories = ["Qld", "NSW", "Vic", "WA", "SA", "NT", "Tas", "ACT"]
+	options2.xaxis.title.text = "State"
+	options2.yaxis.title.text = "Tonnes"
+	options2.yaxis.labels.formatter = val => `${Math.round(val)}M`
+	options2.tooltip.y = {
+		formatter: val => `${(val * 1000000).toLocaleString()}`
+	}
+
+	soefinding.state.chart2 = {
+		options: options2,
+		series: sectorSeries,
+		chartactive: true,
+	};
+
+
+	//3. multi line, trend in qld sectors
+	const qldTotal = soefinding.findingJson.data.find(d => d.State == "Queensland" && d.Sector == "All (incl. LULUCF)")
+	qldSectors.unshift(qldTotal)
+	const qldTrendSeries = qldSectors.map(d => {
+		return {
+			name: d.Sector,
+			data: yearKeys.map(y => d[y])
+		}
+	})
+
+	const options3 = soefinding.getDefaultLineChartOptions()
+	options3.markers = { size: 4 }
+	options3.xaxis.categories = ["Year", "Emissions (tonnes)"]
+	options3.xaxis.title.text = "Year"
+	options3.yaxis.title.text = "Tonnes"
+	options3.yaxis.labels.formatter = val => `${Math.round(val)}M`
+	options3.tooltip.y = {
+		formatter: val => `${(val * 1000000).toLocaleString()}`
+	}
+
+	soefinding.state.chart3 = {
+		options: options3,
+		series: qldTrendSeries,
+		chartactive: true,
+	};
+
+
+	//4. table only, qld totals
+	const qldTotalSeries = yearKeys.map(y => qldTotal[y])
+
+	const options4 = {
+		xaxis: { categories: yearKeys },
+		labels: yearKeys
+	}
+
+	soefinding.state.chart4 = {
+		options: options4,
+		series: qldTotalSeries
+	};
+
+
 
 	new Vue({
 		el: "#chartContainer",
 		data: soefinding.state,
 		computed: {
 			heading1: () => `Proportion of Queensland’s emissions by sector, ${latestYear}`,
+			heading2: () => `Comparison of state and territory emissions by sector, ${latestYear}`,
+			heading3: () => `Trends in Queensland emissions, by sector`,
+			heading4: () => "Total Queensland emissions"
 		},
 		methods: {
-			formatter1: val => val.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })
+			formatter1: val => val.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 }) //reüse for 2, 3
 		}
 	})
 })
