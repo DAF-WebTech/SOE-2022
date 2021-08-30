d438474741083f4ec8ab6dbb50a5c3966cbacaae
PORTER David <david.porter@daf.qld.gov.au>
2021-08-30 16:26:58 +1000

    add charts

--- src/pollution/waste/interstate-household-waste.js
+++ src/pollution/waste/interstate-household-waste.js
@@ -2,10 +2,11 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-	const yearKeys = soefinding.findingJson.meta.fields.slice(1, 5)
+	const yearKeys = soefinding.findingJson.meta.fields.slice(1, 6)
 
 	// 1. stacked column, waste type yearly
-	const wasteTypeSeries = soefinding.findingJson.data.map(d => {
+	const sectorItems = soefinding.findingJson.data.filter(d => d["Waste type"] !== "All")
+	const wasteTypeSeries = sectorItems.map(d => {
 		return {
 			name: d["Waste type"],
 			data: yearKeys.map(y => d[y])
@@ -13,12 +14,13 @@ document.addEventListener("DOMContentLoaded", function () {
 	})
 
 	const options1 = soefinding.getDefaultBarChartOptions()
-	options1.xaxis.categories = yearKeys
+	options1.chart.stacked = true
+	options1.xaxis.categories = yearKeys.map(y => y.replace("-", "–")) // ndash
 	options1.xaxis.title.text = "Year"
 	options1.yaxis.title.text = "Tonnes (million)"
-	options1.yaxis.labels.formatter = val => `${Math.round(val)}M`
+	options1.yaxis.labels.formatter = val => `${(val / 1000000)}M`
 	options1.tooltip.y = {
-		formatter: val => `${(val * 1000000)?.toLocaleString() ?? "n/a"}`
+		formatter: val => `${(val)?.toLocaleString() ?? "n/a"}`
 	}
 
 	soefinding.state.chart1 = {
@@ -28,16 +30,93 @@ document.addEventListener("DOMContentLoaded", function () {
 	};
 
 
+	//2. line chart, total items by year
+	const allItem = soefinding.findingJson.data.find(d => d["Waste type"] == "Municipal solid")
+	const totalItemSeries = [{ name: "Tonnes", data: yearKeys.map(y => allItem[y]) }]
+
+	const options2 = soefinding.getDefaultLineChartOptions()
+	options2.chart.stacked = true
+	options2.xaxis.categories = yearKeys.map(y => y.replace("-", "–")) // ndash
+	options2.xaxis.title.text = "Year"
+	options2.yaxis.title.text = "Tonnes"
+	options2.yaxis.tickAmount = 7
+	options2.yaxis.min = 40000
+	options2.yaxis.max = 70000
+	options2.yaxis.labels.formatter = val => `${(val.toLocaleString())}`
+	options2.tooltip.y = {
+		formatter: val => `${val}`
+	}
+
+	soefinding.state.chart2 = {
+		options: options2,
+		series: totalItemSeries,
+		chartactive: true,
+	};
+
+
+	// 3. pie lastest other items
+	const latestOtherSeries = sectorItems.map(d => d["2018-19 Other"] ? d["2018-19 Other"] : 0)
+	latestOtherSeries.sort(function (a, b) {
+		return b - a
+	})
+
+	const options3 = soefinding.getDefaultPieChartOptions()
+	// the pie charts uses labels, but the table vue is looking for categories
+	options3.labels = sectorItems.map(d => d["Waste type"])
+	options3.tooltip = {
+		y: {
+			formatter: (val, options) => {
+				const percent = options.globals.seriesPercent[options.seriesIndex][0]
+				return `${val.toLocaleString()} (${percent.toFixed(1)}%)`
+			}
+		}
+	}
+	options3.xaxis.categories = ["Waste Type", "Tonnes"] // not needed for chart, but vue uses them for table headings
+
+	soefinding.state.chart3 = {
+		options: options3,
+		series: latestOtherSeries,
+		chartactive: true,
+	};
+
+
+	// 4 stacked columns, latest year, other and landfill
+	const latestOtherLandfillSeries = sectorItems.map(d => {
+		return {
+			name: d["Waste type"],
+			data: [d["2018-19 Received by Landfill"], d["2018-19 Other"]]
+		}
+	})
+
+	const options4 = soefinding.getDefaultBarChartOptions()
+	options4.chart.stacked = true
+	options4.xaxis.categories = ["2018-19 Received by Landfill", "2018-19 Other"]
+	options4.xaxis.title.text = "Type of interstate household waste received"
+	options4.yaxis.title.text = "Tonnes"
+	options4.yaxis.labels.formatter = val => val
+	options4.tooltip.y = {
+		formatter: val => `${(val)?.toLocaleString() ?? "n/a"}`
+	}
+
+	soefinding.state.chart4 = {
+		options: options4,
+		series: latestOtherLandfillSeries,
+		chartactive: true,
+	};
+
+
+
 	new Vue({
 		el: "#chartContainer",
 		data: soefinding.state,
 		computed: {
 			heading1: () => "Interstate household waste received, by waste type",
-			heading2: () => "Trends in Queensland's net land use, land use change and forestry (LULUCF) emissions, by category",
-			heading3: () => "Queensland’s total land use, land use change and forestry (LULUCF) emissions",
+			heading2: () => "Trend in total interstate household waste received",
+			heading3: () => "Proportion of interstate household waste received, 2018–19 Other",
+			heading4: () => "Interstate household waste received by landfill and other, 2018–2019"
 		},
 		methods: {
-			formatter1: val => val.toLocaleString(undefined, { minimumFractionDigits: 3, maximumFractionDigits: 3 })
+			formatter1: val => val?.toLocaleString() ?? ""
 		}
 	})
 })
