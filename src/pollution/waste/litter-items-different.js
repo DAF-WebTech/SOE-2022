b2449ba9b78759d4ea8de29021489cc9397c92ff
David Porter <david.porter@daf.qld.gov.au>
2021-08-31 14:26:14 +1000

    add two bottom charts

--- src/pollution/waste/litter-items-different.js
+++ src/pollution/waste/litter-items-different.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	const yearKeys = soefinding.findingJson.meta.fields.slice(2)
 	const latestYear = yearKeys[yearKeys.length - 1]
 
-	// 1. bar, average litter count by site type
+	// 1. column, average litter count by site type
 	const extents = ["Australia", "Queensland"]
 	const averageSeries = extents.map (e => {
 		return {
@@ -14,11 +14,10 @@ document.addEventListener("DOMContentLoaded", function () {
 		}
 	})
 
-	const options1 = soefinding.getDefaultLineChartOptions()
+	const options1 = soefinding.getDefaultBarChartOptions()
 	options1.xaxis.title.text = "Site type"
-	options1.xaxis.categories = soefinding.findingJson.data.filter(d => d.Extent == "Queensland").map(d => d.Extent)
+	options1.xaxis.categories = soefinding.findingJson.data.filter(d => d.Extent == "Queensland").map(d => d.Site.split(" "))
 	options1.yaxis.title.text = "Number of items per 100m²"
-
 
 	soefinding.state.chart1 = {
 		options: options1,
@@ -27,18 +26,42 @@ document.addEventListener("DOMContentLoaded", function () {
 	};
 
 
+  //2. column difference in percentage
+  const sites = soefinding.findingJson.data.filter(d => d.Extent == "Queensland").map(d => d.Site)
+  const percentSeries = [{ name: "Percentage", data: sites.map(s => {
+  	  const extents = soefinding.findingJson.data.filter(d => d.Site == s)
+  	  return Math.round(100.0 - (extents[0][latestYear] / extents[1][latestYear] * 100.0))
+  })  }]
+
+  const options2 = JSON.parse(JSON.stringify(options1))
+  options2.yaxis.title.text = "Percentage difference in count (%)"
+      options2.tooltip = {
+		y: {
+			formatter: function (val) {
+				return `${val < 1 ? '−' : ''}${Math.abs(val)}%` // a better minus sign
+			}
+		}
+	};
+
+
+	soefinding.state.chart2 = {
+		options: options2,
+		series: percentSeries,
+		chartactive: true,
+	};
+
 
 
 	new Vue({
 		el: "#chartContainer",
 		data: soefinding.state,
 		computed: {
-			heading1: () => `Average litter count by site type, ${latestYear}`,
-			heading2: () => "Trends in volume of litter items per 1000m² by Queensland and Australia",
+			heading1: () => `Average litter count by site type, ${latestYear.replace("-", "–")}`,
+			heading2: () => `Percentage difference between Queensland and Australian counts by site type, ${latestYear.replace("-", "–")}`,
 		},
 		methods: {
 			formatter1: val => val,
-			formatter2: val => val.toLocaleString(undefined, {minimumFractionDigits: 2}),
+			formatter2: val => `${val < 1 ? '−' : ''}${Math.abs(val)}`, // a better minus sign
 		}
 	})
 })
