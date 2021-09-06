af685816140459a1602a0af2090ce30f151171ae
PORTER David <david.porter@daf.qld.gov.au>
2021-09-06 15:37:59 +1000

    small fixes

--- src/pollution/waste/per-capita-waste.js
+++ src/pollution/waste/per-capita-waste.js
@@ -7,21 +7,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	//1. column, all waste types each year
 	const qldItem = soefinding.findingJson.data.find(d => d["Waste region"] == "Queensland")
-	const wasteYearSeries = {
-		name: "Total",
+	const wasteYearSeries = [{
+		name: "Kilograms per capita",
 		data: yearKeys.slice(8).map(y => qldItem[y])
-	}
+	}]
 
 	const options1 = soefinding.getDefaultBarChartOptions()
 	options1.xaxis.categories = yearKeys.slice(8).map(y => y.replace("-", "–")) // ndash
 	options1.xaxis.title.text = "Year"
 	options1.yaxis.title.text = "Kilograms per capita"
-	options1.yaxis.labels.formatter = val => {
-		return `${(val / 1000000).toFixed(1)}M`
-	}
-	options1.tooltip.y = {
-		formatter: val => val.toLocaleString()
-	}
 
 	soefinding.state.chart1 = {
 		options: options1,
@@ -31,38 +25,38 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 	// 2. line, same data as (1), all years
-	const trendSeries = {
-		name: "Total",
+	const trendSeries = [{
+		name: "Kilograms per capita",
 		data: yearKeys.map(y => qldItem[y])
-	}
+	}]
 
 	const options2 = soefinding.getDefaultLineChartOptions()
 	options2.xaxis.categories = yearKeys.map(y => y.replace("-", "–")) // ndash
 	options2.xaxis.title.text = "Year"
 	options2.yaxis.title.text = "Kilograms per capita"
-	options2.yaxis.labels.formatter = val => {
-		return `${(val / 1000000).toFixed(1)}M`
-	}
-	options2.tooltip.y = {
-		formatter: val => val.toLocaleString()
-	}
 
 	soefinding.state.chart2 = {
 		options: options2,
-		series: qldAllSeries,
+		series: trendSeries,
 		chartactive: true,
 	}
 
 
 	// 3 column, total in latest year for each region
 	const regionItems = soefinding.findingJson.data.filter(d => d["Waste region"] != "Queensland")
-	const regionSeries = regionItems.map(d => d[latestYear])
+	regionItems.sort(function (a, b) {
+		return b[latestYear] - a[latestYear]
+	})
+	const regionSeries = [{
+		name: "Kilograms per capita",
+		data: regionItems.map(d => d[latestYear])
+	}]
 	regionSeries.sort(function (a, b) {
 		return b - a
 	})
 
 	const options3 = soefinding.getDefaultBarChartOptions()
-	options3.xaxis.categories = regionItems.map(d => d["Waste region"].replace("-", "–"))
+	options3.xaxis.categories = regionItems.map(d => d["Waste region"].replace("-", " –").split(" "))
 	options3.xaxis.title.text = "Region"
 	options3.yaxis.title.text = "Kilograms per capita"
 
@@ -79,9 +73,9 @@ document.addEventListener("DOMContentLoaded", function () {
 		el: "#chartContainer",
 		data: soefinding.state,
 		computed: {
-			heading1: () => "Household waste recovered, by collection type",
-			heading2: () => "Trend in total household waste recovered",
-			heading3: () => `Proportion of household waste recovered by region, ${latestYear.replace("-", "–")}`, //ndash
+			heading1: () => "Household waste generated per capita",
+			heading2: () => "Trend in total household waste generated per capita",
+			heading3: () => `Househould waste generated per capita by region, ${latestYear.replace("-", "–")}`, //ndash
 		},
 		methods: {
 			formatter1: val => val?.toLocaleString() ?? "",
