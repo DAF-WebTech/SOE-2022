1e843fc70a3b3870ed2c8b228c0fe0a640105199
PORTER David <david.porter@daf.qld.gov.au>
2021-09-07 09:16:54 +1000

    all charts

--- src/pollution/waste/construction-demolition-landfilled.js
+++ src/pollution/waste/construction-demolition-landfilled.js
@@ -8,10 +8,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	//1. column chart for qld for the last four years
 	const qldItem = soefinding.findingJson.data.find(d => d["Waste region"] == "Queensland")
-	const fourYearSeries = {
+	const fourYearSeries = [{
 		name: "Queensland",
 		data: lastFourYears.map(y => qldItem[y])
-	}
+	}]
 
 	const options1 = soefinding.getDefaultBarChartOptions()
 	options1.xaxis.categories = lastFourYears.map(y => y.replace("-", "–")) // ndash
@@ -31,21 +31,55 @@ document.addEventListener("DOMContentLoaded", function () {
 	}
 
 
+	// 2. line chart for qld for all years
+	const allYearSeries = [{
+		name: "Queensland",
+		data: yearKeys.map(y => qldItem[y])
+	}]
+
+	const options2 = soefinding.getDefaultLineChartOptions()
+	options2.xaxis.categories = yearKeys.map(y => y.replace("-", "–")) // ndash
+	options2.xaxis.title.text = "Year"
+	options2.yaxis.title.text = "Tonnes (million)"
+	options2.yaxis.labels.formatter = val => {
+		return `${(val / 1000000).toFixed(1)}M`
+	}
+	options2.tooltip.y = {
+		formatter: val => val.toLocaleString()
+	}
+
+	soefinding.state.chart2 = {
+		options: options2,
+		series: allYearSeries,
+		chartactive: true,
+	}
 
 
+	//3. pie chart for all regions, latest Year
+	const regionItems = soefinding.findingJson.data.filter(d => d["Waste region"] != "Queensland")
+	regionItems.sort(function (a, b) {
+		return b[latestYear] - a[latestYear]
+	})
+	const regionSeries = regionItems.map(d => d[latestYear])
 
+	const options3 = soefinding.getDefaultPieChartOptions()
+	options3.xaxis.categories = ["Waste Region", "Tonnes"] //ndash
+	options3.labels = regionItems.map(d => d["Waste region"].replace("-", "–"))
 
-
-
+	soefinding.state.chart3 = {
+		options: options3,
+		series: regionSeries,
+		chartactive: true,
+	}
 
 
 	new Vue({
 		el: "#chartContainer",
 		data: soefinding.state,
 		computed: {
-			heading1: () => "Household waste landfilled, by collection type",
-			heading2: () => "Trend in total household waste landfilled",
-			heading3: () => `Proportion of household waste landfilled by region, ${latestYear.replace("-", "–")}`, //ndash
+			heading1: () => "Construction and demolition waste landfilled",
+			heading2: () => "Trend in total construction and demolition waste landfilled",
+			heading3: () => `Proportion of construction and demolition waste landfilled by region, ${latestYear.replace("-", "–")}`, //ndash
 		},
 		methods: {
 			formatter1: val => val?.toLocaleString() ?? "",
