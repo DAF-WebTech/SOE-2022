f28089224dfca3dbe5582b6dca12d5f1ed37bff5
PORTER David <david.porter@daf.qld.gov.au>
2021-09-10 12:41:26 +1000

    alll charts

--- src/climate/coasts-oceans/sea-surface-temperature.js
+++ src/climate/coasts-oceans/sea-surface-temperature.js
@@ -3,47 +3,61 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 	const locations = ["Coral Sea", "Northern Tropics"]
+
 	const options1 = soefinding.getDefaultBarChartOptions()
 	options1.xaxis.title.text = "Year"
 	options1.yaxis.title.text = "Temperature change (degrees celsius)"
+	options1.yaxis.tickAmount = 4
+	options1.yaxis.min = -1
+	options1.yaxis.max = 1
+	options1.yaxis.labels.formatter = val => val < 0 ? `−${Math.abs(val).toFixed(1)}` : val.toFixed(1)
+	options1.tooltip.y = {
+		formatter: val => val < 0 ? `−${Math.abs(val)}` : val
+	}
+
 	const options2 = JSON.parse(JSON.stringify(options1))
+	//options2.forceNiceScale = false
+	options2.yaxis.tickAmount = 6
+	options2.yaxis.min = -0.6
+	options2.yaxis.max = 0.6
+	options2.yaxis.labels.formatter = options1.yaxis.labels.formatter
+	options2.tooltip.y.formatter = options1.tooltip.y.formatter
 
 	locations.forEach((loc, i) => {
 
 		// chart 1, column, Annual mean sea surface temperature anomaly
 		const anomalyItems = soefinding.findingJson.data.filter(d => d.Location == loc)
 		const anomalyYears = []
-		const anomalySeries = {
+		const anomalySeries = [{
 			name: "Temperature anomaly",
-			series: anomalyItems.map(d => {
+			data: anomalyItems.map(d => {
 				anomalyYears.push(d.Year) // side effect
 				return d["Annual sea surface temperature anomaly"]
 			})
-		}
+		}]
 
 		options1.xaxis.categories = anomalyYears
 
-		soefinding.state[`chart${i + 1}`] = {
+		soefinding.state[`chart${i * 2 + 1}`] = {
 			options: options1,
 			series: anomalySeries,
 			chartactive: true,
 		};
 
-
-		// chart 2, column, Annual mean sea surface temperature 10-year rolling average
+		//chart 2, column, Annual mean sea surface temperature 10-year rolling average
 		const annualMeanItems = anomalyItems.filter(d => d["Ten year rolling average"] != null)
 		const annualMeanYears = []
-		const annualMeanSeries = {
+		const annualMeanSeries = [{
 			name: "Ten-year rolling average",
-			series: annualMeanItems.map(d => {
+			data: annualMeanItems.map(d => {
 				annualMeanYears.push(d.Year) // side effect
 				return d["Ten year rolling average"]
 			})
-		}
+		}]
 
 		options2.xaxis.categories = annualMeanYears
 
-		soefinding.state[`chart${i + 1}`] = {
+		soefinding.state[`chart${i * 2 + 2}`] = {
 			options: options2,
 			series: annualMeanSeries,
 			chartactive: true,
@@ -61,7 +75,8 @@ document.addEventListener("DOMContentLoaded", function () {
 			heading4: function () { return `${locations[1]} — Annual mean sea surface temperature 10-year rolling average` },
 		},
 		methods: {
-			formatter1: val => val
+			formatter1: val => val < 0 ? `−${Math.abs(val).toFixed(2)}` : val.toFixed(2),
+			formatter2: val => val < 0 ? `−${Math.abs(val).toFixed(3)}` : val.toFixed(3)
 		}
 	});
 })
