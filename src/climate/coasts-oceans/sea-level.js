e8f825baacc95fd063e711db71df704ddd048c0d
PORTER David <david.porter@daf.qld.gov.au>
2021-09-13 12:11:29 +1000

    typos

--- src/climate/coasts-oceans/sea-level.js
+++ src/climate/coasts-oceans/sea-level.js
@@ -2,27 +2,27 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-	const measures = soefinding.findingJson.meta.fields.slice(2, 3)
+	const measures = soefinding.findingJson.meta.fields.slice(2)
 
 	const series = measures.map(m => {
 		return {
 			name: m,
 			data: soefinding.findingJson.data.map(d => {
-				//return [new Date(d.Year, d.Month-1).getTime(), d[m]]
-				return { x: `${d.Year}-${d.Month.toString().padStart(2, '0')}-01`, y: d[m] }
+				return [new Date(d.Year, d.Month - 1).getTime(), d[m]]
+				//return { x: `${d.Year}-${d.Month.toString().padStart(2, '0')}-01`, y: d[m] }
 			})
 		}
 	})
 
 	const options = soefinding.getDefaultLineChartOptions()
+	delete options.xaxis.categories
 	options.xaxis.type = "datetime"
 	options.xaxis.title.text = "Date"
 	options.yaxis.title.text = "Mean sea level change (mm)"
-	//options.yaxis.labels.formatter = val => val < 0 ? `−${Math.abs(val).toFixed(0)}` : val.toFixed(0)
-	//options.yaxis.labels.formatter = val => val < 0 ? `−${Math.abs(val).toFixed(0)}` : val.toFixed(0)
-	//options.tooltip.y = {
-	//	formatter: val => val < 0 ? `−${Math.abs(val)}` : val
-	//}
+	options.yaxis.labels.formatter = val => val < 0 ? `−${Math.abs(val).toFixed(0)}` : val.toFixed(0)
+	options.tooltip.y = {
+		formatter: val => val < 0 ? `−${Math.abs(val)}` : val
+	}
 
 	soefinding.state.chart1 = {
 		options: options,
