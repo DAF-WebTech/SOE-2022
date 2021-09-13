dea4b03894632d8dbe51d2bfa7ecae61dd51a4c4
PORTER David <david.porter@daf.qld.gov.au>
2021-09-13 14:26:48 +1000

    fix options and typos

--- src/climate/coasts-oceans/storm-tide-inundation.js
+++ src/climate/coasts-oceans/storm-tide-inundation.js
@@ -10,27 +10,67 @@ document.addEventListener("DOMContentLoaded", function () {
 		cyclones[d.Cyclone].push(d)
 	})
 
-	const keys = soefinding.findingJson.meta.fields.slice(2)
 
+	const keys = soefinding.findingJson.meta.fields.slice(3)
 	const options = soefinding.getDefaultBarChartOptions()
+	options.xaxis.title.text = "Incident"
+	options.yaxis.title.text = "Metres"
+	options.yaxis.forceNiceScale = false
+
 
 	Object.keys(cyclones).forEach((c, i) => {
 		const data = [[], []]
 		const names = []
 		cyclones[c].forEach((d, i) => {
-			names.push(`${d.Location}, ${d.Date}`)
+			const date = new Date(d.Date)
+			names.push([d.Location, `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`])
 			data[0].push(d[keys[0]])
 			data[1].push(d[keys[1]])
 		})
-		const series = names.map((n, i) => {
+		const series = keys.map((k, i) => {
 			return {
-				name: n,
+				name: k.substring(0, k.indexOf("(")),
 				data: data[i]
 			}
 		})
 
+		const myOptions = JSON.parse(JSON.stringify(options))
+		myOptions.xaxis.categories = names
+		myOptions.tooltip.y = {
+			formatter: function (val) {
+				return val < 0 ? `−${Math.abs(val)}` : val // use correct unicode
+			},
+		}
+
+
+		switch (i) {
+			case 0:
+				myOptions.yaxis.min = -0.5
+				myOptions.yaxis.max = 2.0
+				myOptions.yaxis.tickAmount = 5
+				myOptions.yaxis.labels.formatter = val => val.toFixed(1)
+				break
+
+			case 1:
+				myOptions.yaxis.min = -0.25
+				myOptions.yaxis.max = 1.25
+				myOptions.yaxis.tickAmount = 6
+				myOptions.yaxis.labels.formatter = val => val.toFixed(2)
+
+				break
+
+			case 2:
+				myOptions.yaxis.min = -0.2
+				myOptions.yaxis.max = 1.0
+				myOptions.yaxis.tickAmount = 6
+				myOptions.yaxis.labels.formatter = val => val.toFixed(1)
+
+				break
+		}
+
+
 		soefinding.state[`chart${i + 1}`] = {
-			options: options,
+			options: myOptions,
 			series: series,
 			chartactive: true,
 		};
@@ -42,13 +82,13 @@ document.addEventListener("DOMContentLoaded", function () {
 		el: "#chartContainer",
 		data: soefinding.state,
 		computed: {
-			heading1: () => "",
-			heading2: () => "",
-			heading3: () => "",
+			heading1: () => `Cyclone ${Object.keys(cyclones)[0]}`,
+			heading2: () => `Cyclone ${Object.keys(cyclones)[1]}`,
+			heading3: () => `Cyclone ${Object.keys(cyclones)[2]}`,
 			heading4: () => "",
 		},
 		methods: {
-			formatter1: val => val
+			formatter1: val => val < 0 ? `−${Math.abs(val)}` : val // use correct unicode
 		}
 	});
 })
