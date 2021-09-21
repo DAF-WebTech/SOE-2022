0756f10e60f382e99108f9f96b6426f461a70a33
PORTER David <david.porter@daf.qld.gov.au>
2021-09-21 12:46:02 +1000

    initial commit

---
+++ src/liveability/urban/use-bikeways-seq.js
@@ -0,0 +1,41 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", function () {
+
+	const years = soefinding.findingJson.meta.fields.slice(1)
+
+	const series = years.map(y => {
+		return {
+			name: y, 
+			data: soefinding.findingJson.data.map(d => d[y])
+		}
+	})
+
+
+	const options = soefinding.getDefaultColumnChartOptions()
+	options.xaxis.categories = soefinding.findingJson.data.map(d => d.Site)
+	options.xaxis.title.text = "Location"
+	options.yaxis.title.text = "Average daily count"
+	// options.yaxis.labels.formatter = val => val
+	// options.tooltip = { y: { formatter: val => val } }
+
+
+	soefinding.state.chart1 = {
+		options: options,
+		series: series,
+		chartactive: true,
+	}
+
+
+	new Vue({
+		el: "#chartContainer",
+		data: soefinding.state,
+		computed: {
+			heading1: () => `Daily use of bikeways ${years[0]} to ${years[years.length-1]}`
+		},
+		methods: {
+			formatter1: val => val?.toLocaleString() ?? ""
+
+		}
+	});
+})
