6ca267a87c2d065b48f3193500d1ad24007df8e7
PORTER David <david.porter@daf.qld.gov.au>
2021-09-21 15:07:54 +1000

    initial commit

---
+++ src/liveability/rural/soil-land-resources.js
@@ -0,0 +1,43 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", function () {
+
+	const keys = soefinding.findingJson.meta.fields.slice(1)
+
+	soefinding.findingJson.forEach(d => {
+		soefinding.findingContent[this.state.currentRegionName].app1 = keys.map(k => d[k])
+	})
+
+
+	var options = soefinding.getDefaultPieChartOptions();
+	options.labels = keys
+	options.xaxis = { categories: "Use", "Hectares" };
+
+	soefinding.state.chart1 = {
+		options: options,
+		series: soefinding.findingContent[soefinding.state.currentRegionName].app1,
+		chartactive: true,
+	};
+
+	new Vue({
+		el: "#chartContainer",
+		data: soefinding.state,
+		computed: {
+			heading1: () => `Proportion of land by use in ${}, 2019`
+		},
+		methods: {
+			formatter1: val => val.toFixed(1)
+		}
+	});
+})
+
+window.soefinding.onRegionChange = function () {
+	// set the data series in each of the vue apps, for the current region
+	soefinding.state.chart1.series =
+		this.findingContent[this.state.currentRegionName].app1;
+
+	soefinding.loadFindingHtml();
+};
+
+
+})
