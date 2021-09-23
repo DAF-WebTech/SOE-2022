70c0b078fca682c22b35960aed00fcde9f292d1b
PORTER David <david.porter@daf.qld.gov.au>
2021-09-23 17:04:01 +1000

    add tfoot calc on qld chart 1

--- src/heritage/historic/queensland-open-house.js
+++ src/heritage/historic/queensland-open-house.js
@@ -5,9 +5,9 @@ soefinding.regions = pinLocations // these should already be set in ssjs
 
 document.addEventListener("DOMContentLoaded", function () {
 
-	const years = [...new Set( soefinding.findingJson.data.map(d => d.Year))]
-	const locations = [...new Set( soefinding.findingJson.data.map(d => d.Location))]
-	
+	const years = [...new Set(soefinding.findingJson.data.map(d => d.Year))]
+	const locations = [...new Set(soefinding.findingJson.data.map(d => d.Location))]
+
 	const qldSeries1 = locations.map(loc => {
 		const locationData = soefinding.findingJson.data.filter(d => d.Location == loc)
 		return {
@@ -16,10 +16,15 @@ document.addEventListener("DOMContentLoaded", function () {
 				const item = locationData.find(ld => ld.Year == y)
 				if (item)
 					return item["Heritage places open"]
-				else 
-				return ""
+				else
+					return ""
 			})
 		}
+	})
+	const tfoot = qldSeries1.map(q => {
+		return q.data.reduce((acc, curr) => {
+			return acc + (Number.isInteger(curr) ? curr : 0)
+		}, 0)
 	})
 
 	const options1 = soefinding.getDefaultStackedColumnChartOptions()
@@ -31,6 +36,8 @@ document.addEventListener("DOMContentLoaded", function () {
 		options: options1,
 		series: qldSeries1,
 		chartactive: true,
+		tfoot: tfoot
+
 	};
 
 
@@ -42,15 +49,15 @@ document.addEventListener("DOMContentLoaded", function () {
 				const item = locationData.find(ld => ld.Year == y)
 				if (item)
 					return item["Visitors"]
-				else 
-				return ""
+				else
+					return ""
 			})
 		}
 	})
 
 	const options2 = JSON.parse(JSON.stringify(options1))
 	options2.yaxis.title.text = "Number of visitors"
-	options2.yaxis.labels.formatter = val => `${val/1000}k`
+	options2.yaxis.labels.formatter = val => `${val / 1000}k`
 	options2.tooltip.y = { formatter: val => val.toLocaleString() }
 
 
@@ -60,16 +67,18 @@ document.addEventListener("DOMContentLoaded", function () {
 		chartactive: true,
 	};
 
-    // now we can interate qld series and pull out info for each city
-    soefinding.findingContent.Queensland = { html: "" } // qld is not in chart3 and 4, but we still need to save its html
+	// now we can interate qld series and pull out info for each city
+	soefinding.findingContent.Queensland = { html: "" } // qld is not in chart3 and 4, but we still need to save its html
 
-    // chart 3, places open
+	// chart 3, places open
 	qldSeries1.forEach(q => {
-		soefinding.findingContent[q.name] = { app3: [{
-			name: q.name,
-			data: q.data,
-			html: null
-		}]}
+		soefinding.findingContent[q.name] = {
+			app3: [{
+				name: q.name,
+				data: q.data,
+				html: null
+			}]
+		}
 		// we have to remove the null items
 		soefinding.findingContent[q.name].categories = []
 		const newData = [] // 
@@ -82,20 +91,20 @@ document.addEventListener("DOMContentLoaded", function () {
 		soefinding.findingContent[q.name].app3[0].data = newData
 	})
 
-    const options3 = JSON.parse(JSON.stringify(options1))
-    options3.chart.id = "chart3"
+	const options3 = JSON.parse(JSON.stringify(options1))
+	options3.chart.id = "chart3"
 	soefinding.state.chart3 = {
 		options: options3,
 		chartactive: true,
 		html: null
 	};
 	if (soefinding.state.currentRegionName == "Queensland")
-	  soefinding.state.chart3.series = soefinding.findingContent.Bundaberg.app3 // set a default
+		soefinding.state.chart3.series = soefinding.findingContent.Bundaberg.app3 // set a default
 	else
-      soefinding.state.chart3.series = soefinding.findingContent[soefinding.state.currentRegionName].app3 
+		soefinding.state.chart3.series = soefinding.findingContent[soefinding.state.currentRegionName].app3
 
 
-    // chart 4, visitors
+	// chart 4, visitors
 	qldSeries2.forEach(q => {
 		soefinding.findingContent[q.name].app4 = [{
 			name: q.name,
@@ -113,17 +122,19 @@ document.addEventListener("DOMContentLoaded", function () {
 		soefinding.findingContent[q.name].app4[0].data = newData
 	})
 
-    const options4 = JSON.parse(JSON.stringify(options2))
-    options4.chart.id = "chart4"
+	const options4 = JSON.parse(JSON.stringify(options2))
+	options4.chart.id = "chart4"
+	options4.yaxis.labels.formatter = options2.yaxis.labels.formatter
+	options4.tooltip.y = options2.tooltip.y
 	soefinding.state.chart4 = {
 		options: options4,
 		chartactive: true,
 		html: null
 	};
 	if (soefinding.state.currentRegionName == "Queensland")
-	  soefinding.state.chart4.series = soefinding.findingContent.Bundaberg.app4 // set a default
+		soefinding.state.chart4.series = soefinding.findingContent.Bundaberg.app4 // set a default
 	else
-      soefinding.state.chart4.series = soefinding.findingContent[soefinding.state.currentRegionName].app4 
+		soefinding.state.chart4.series = soefinding.findingContent[soefinding.state.currentRegionName].app4
 
 
 	new Vue({
@@ -134,7 +145,7 @@ document.addEventListener("DOMContentLoaded", function () {
 			heading2: () => `People visiting heritage places in ${soefinding.state.currentRegionName}`
 		},
 		methods: {
-			formatter1: val =>  isNaN(parseInt(val)) ? "" : val.toLocaleString(),
+			formatter1: val => isNaN(parseInt(val)) ? "" : val.toLocaleString(),
 		}
 	})
 
@@ -144,26 +155,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
 			// update chart 3
 			this.state.chart3.series = this.findingContent[this.state.currentRegionName].app3
-			
+
 			// this works on the table
 			soefinding.state.chart3.options.xaxis.categories = this.findingContent[this.state.currentRegionName].categories
 			// but we also need this for the chart to update
 			ApexCharts.exec("chart3", "updateOptions", {
-				xaxis: { categories: this.findingContent[this.state.currentRegionName].categories}
+				xaxis: { categories: this.findingContent[this.state.currentRegionName].categories }
 			})
 
 
-              // update chart 4
+			// update chart 4
 			this.state.chart4.series = this.findingContent[this.state.currentRegionName].app4
 			// this works on the table
 			soefinding.state.chart4.options.xaxis.categories = this.findingContent[this.state.currentRegionName].categories
 			// but we also need this for the chart to update
 			ApexCharts.exec("chart4", "updateOptions", {
-				xaxis: { categories: this.findingContent[this.state.currentRegionName].categories}
+				xaxis: { categories: this.findingContent[this.state.currentRegionName].categories }
 			})
 
 		}
-		
+
 		soefinding.loadFindingHtml();
 	}
 
