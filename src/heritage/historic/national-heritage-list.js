325d3c75da94624990234124979f2e6a53f77c3e
PORTER David <david.porter@daf.qld.gov.au>
2021-09-24 15:45:43 +1000

    fixes

--- src/heritage/historic/national-heritage-list.js
+++ src/heritage/historic/national-heritage-list.js
@@ -12,6 +12,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	const options1  = soefinding.getDefaultColumnChartOptions() 
 	options1.xaxis.categories = years
+	options1.xaxis.title.text = "Year"
+	options1.yaxis.title.text = "Number"
 
 		soefinding.state.chart1 = {
 		options: options1,
@@ -19,6 +21,19 @@ document.addEventListener("DOMContentLoaded", function () {
 		chartactive: true,
 	}
 
+
+	const series2 = [soefinding.findingJson.data[0].Entered, soefinding.findingJson.data[0].Removed]
+
+	const options2 = { 
+		xaxis: { categories: ["Register<br>Change", "Number<br>of Places"] },
+		labels: ["Entered", "Removed"]
+	}
+
+	soefinding.state.chart2 = {
+		options: options2,
+		series: series2,
+		chartactive: false,
+	}
 
 
 
@@ -29,9 +44,8 @@ document.addEventListener("DOMContentLoaded", function () {
 		el: "#chartContainer",
 		data: soefinding.state,
 		computed: {
-			heading1: () => `Total places removed, ${years[0]}–${latestYear}`,
-			heading2: () => `Proportion of places removed by Local Government Area (LGA), ${years[0]}–${latestYear}`,
-			heading3: () => `Places removed in each LGA, ${years[0]}–${latestYear}`
+			heading1: () => `Number of places on the National Heritage Register in Queensland`,
+			heading2: () => `Number of places entered or removed from the heritage register, ${years[0]}–${years.at(-1)}`
 		},
 		methods: {
 			formatter1: val => val,
