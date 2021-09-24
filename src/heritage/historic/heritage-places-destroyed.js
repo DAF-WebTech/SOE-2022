5b87044e0158eddaa01e149db600e0161e135f23
PORTER David <david.porter@daf.qld.gov.au>
2021-09-24 15:13:50 +1000

    add extra charts

--- src/heritage/historic/heritage-places-destroyed.js
+++ src/heritage/historic/heritage-places-destroyed.js
@@ -3,28 +3,77 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	const years = soefinding.findingJson.meta.fields.slice(1)
 	const latestYear = years.at(-1)
-	
-	// add a total property to each
+
+	//will use for series 1
+	const yearsTotals = years.map(y=> 0)
+	let total = 0
+
 	soefinding.findingJson.data.forEach(d => {
-		d.Total = years.map((acc, curr) => {
-			return acc + d[curr]
+		// create a total property for each lga
+		d.Total = years.reduce((accumulator, current) => {
+			return accumulator + d[current]
 		}, 0)
+
+		// get year totals for table 1
+		years.forEach((y, i) => {
+			yearsTotals[i] += d[y]
+			total += d[y]
+		})
+	
 	})
 
-	const series1items = soefinding.findingJson.data.filter(d => d.Total > 0)
-	const series1data = series1items.map(d => d.Total)
+	//sort
+	soefinding.findingJson.data.sort(function(a, b) {
+		return b.Total - a.Total
+	})	
 
-	const options1 = soefinding.getDefaultPieChartOptions()
-	options1.labels = series1items.map(d => d.LGA)
-	options.xaxis = { categories: ["LGA", "Total"] }
+
+	// series 1 table
+	const series1 = [...yearsTotals, total]
+	const options1 = { 
+		labels: [...years, "Total"],
+		xaxis: { categories: ["Year", "Places<br>removed"] }
+	}
 
 	soefinding.state.chart1 = {
 		options: options1,
-		series: series1data,
+		series: series1,
+		chartactive: false,
+	}
+
+
+	// series 2 pie chart
+	const series2items = soefinding.findingJson.data.filter(d => d.Total > 0)
+	const series2data = series2items.map(d => d.Total)
+
+	const options2 = soefinding.getDefaultPieChartOptions()
+	options2.labels = series2items.map(d => d.LGA)
+	options2.xaxis = { categories: ["LGA", "Total"] }
+
+	soefinding.state.chart2 = {
+		options: options2,
+		series: series2data,
 		chartactive: true,
 	};
 
 
+	// series 3 table
+	const series3 = soefinding.findingJson.data.map(d => {
+		return {
+			name: d.LGA,
+			data: [...years.map(y => d[y]), d.Total]
+		}
+	}) 
+
+	const options3 = { 
+		xaxis: { categories: [...years, "Total"] }
+	}
+
+	soefinding.state.chart3 = {
+		options: options3,
+		series: series3,
+		chartactive: false,
+	};
 
 
 
@@ -32,11 +81,12 @@ document.addEventListener("DOMContentLoaded", function () {
 		el: "#chartContainer",
 		data: soefinding.state,
 		computed: {
-			heading1: () => `Heritage places open in ${soefinding.state.currentRegionName}`,
-			heading2: () => `People visiting heritage places in ${soefinding.state.currentRegionName}`
+			heading1: () => `Total places removed, ${years[0]}–${latestYear}`,
+			heading2: () => `Proportion of places removed by Local Government Area (LGA), ${years[0]}–${latestYear}`,
+			heading3: () => `Places removed in each LGA, ${years[0]}–${latestYear}`
 		},
 		methods: {
-			formatter1: val => isNaN(parseInt(val)) ? "" : val.toLocaleString(),
+			formatter1: val => val,
 		}
 	})
 
