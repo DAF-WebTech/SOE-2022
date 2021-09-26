2bd025de975d14b766f26a8e488866639f62c288
PORTER David <david.porter@daf.qld.gov.au>
2021-09-27 09:18:29 +1000

    initial comit

---
+++ src/biodiversity/terrestrial-ecosystems/extent-change-protected.js
@@ -0,0 +1,69 @@
+"use strict";
+
+
+var fields = soefinding.findingJson.meta.fields.slice(2);
+
+soefinding.yearKeys = [];
+for (var i = 0; i < fields.length; i += 3) {
+	soefinding.yearKeys.push(fields[i].split(" ")[0]);
+}
+
+soefinding.state.chart1 = { series: [], chartactive: true }
+soefinding.state.chart2 = { series: [], chartactive: true }
+soefinding.findingJson.data.forEach(function (row) {
+	if (row["Protected areas"] == "All") {
+		var data = [];
+		fields.forEach(function (field, index) {
+			if (index % 3 == 1) {
+				data.push(row[field]);
+			}
+		})
+		soefinding.state.chart2.series.push({ name: row["Type"], data: data });
+	}
+	else {
+		var data = [];
+		fields.forEach(function (field, index) {
+			if (index % 3 == 0) {
+				data.push(row[field]);
+			}
+		})
+		soefinding.state.chart1.series.push({ name: row["Protected areas"], data: data });
+	}
+});
+
+
+
+soefinding.state.chart1.options = this.getDefaultBarChartOptions();
+soefinding.state.chart1.options.chart.stacked = "true";
+soefinding.state.chart1.options.xaxis.categories = soefinding.yearKeys;
+soefinding.state.chart1.options.xaxis.title.text = "Year";
+soefinding.state.chart1.options.yaxis.title.text = "Hectares";
+soefinding.state.chart1.options.tooltip.y = {
+	formatter: val => val.toLocaleString() + " ha"
+}
+
+
+soefinding.state.chart2.options = this.getDefaultBarChartOptions();
+soefinding.state.chart2.options.chart.stacked = "true";
+soefinding.state.chart2.options.xaxis.categories = soefinding.yearKeys;
+soefinding.state.chart2.options.xaxis.title.text = "Year";
+soefinding.state.chart2.options.yaxis.title.text = "Hectares";
+soefinding.state.chart2.options.tooltip.y = {
+	formatter: val => val.toLocaleString() + " ha"
+}
+
+soefinding.state.chart2.options.yaxis.labels.formatter = val => (val / 1000000) + "M"
+}
+
+
+new Vue({
+	el: "#chartContainer",
+	data: soefinding.state,
+	computed: {
+		heading1: () => `Cumulated number of each protected area`,
+		heading2: () => `Cumulated extent of all protected areas`
+	}
+})
+
+
+
