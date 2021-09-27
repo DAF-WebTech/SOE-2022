aee8ea8a2dc997fc9a71359f36dcb84c6fd90d11
PORTER David <david.porter@daf.qld.gov.au>
2021-09-27 11:41:28 +1000

    simplify for special case

--- src/heritage/world/gbr-heritage-condition.js
+++ src/heritage/world/gbr-heritage-condition.js
@@ -1,32 +1,15 @@
 "use strict"
 document.addEventListener("DOMContentLoaded", function () {
 
-	const series = [ {
-		name: null,
-		data: soefinding.findingJson.data.map(d => d["Criteria summary"])
-	}
-]
-
-	const options = {
-		xaxis: { categories: ["World Heritage natural criteria", "Condition summary", "Condition grade"] }
-	}
-
-	soefinding.state.chart1 = {
-		options: options,
-		series: series,
-		chartactive: false,
+	const series =  {
+		tableHeadings: ["World Heritage natural criteria", "Condition summary", "Condition grade"],
+		data: soefinding.findingJson.data
 	}
 
 
 	new Vue({
 		el: "#chartContainer",
-		data: soefinding.state,
-		computed: {
-			heading1: () => `Great Barrier Reef World Heritage natural criteria condition`
-		},
-		methods: {
-			formatter1: val => val,
-		}
+		data: series
 	})
 
 })
