3ddea64e99ebdc2c40c7599efcabcf05b766166c
PORTER David <david.porter@daf.qld.gov.au>
2021-09-28 16:09:54 +1000

    chart 1 nearly done

--- src/biodiversity/terrestrial-ecosystems/broad-vegetation-groups.js
+++ src/biodiversity/terrestrial-ecosystems/broad-vegetation-groups.js
@@ -2,36 +2,67 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-	const fields =  soefinding.findingJson.meta.fields.slice(-2) // last 2
-	soefinding.findingContent.Queensland = { series1: [] }
-	soefinding.findingJson.data.forEach(d => {
-		if (!soefinding.findingContent[d.Bioregion])
-			soefinding.findingContent[d.Bioregion] = { series1: [] }
-		
-			d.name = `${d["Broad vegetation group number"]}. ${d["Broad vegetation group label"]}` 
-			soefinding.findingContent[d.Bioregion].series1.push( {
-				name: d.name,
-				data: fields.map(f => d[f])
-			})
-
-			let qldItem = soefinding.findingContent.Queensland.series1.find(q => q.name == d.name)
-			if (! qldItem) {
-				qldItem =  {
-						name: d.name, 
-						data: fields.map(f => 0)
-					} 
-					soefinding.findingContent.Queensland.series1.push(qldItem)
-				}
-			fields.forEach(f => qldItem[f] += d[f])
+	soefinding.findingJson.data.sort(function (a, b) {
+		return a["Broad vegetation group number"] - b["Broad vegetation group number"]
 	})
 
+
+
+	const bioregions = {}
+	soefinding.findingJson.data.forEach(d => {
+
+		d.name = `${d["Broad vegetation group number"]}. ${d["Broad vegetation group label"].replace("-", "—")}` // em dash
+
+		if (!bioregions[d.Bioregion])
+			bioregions[d.Bioregion] = []
+		bioregions[d.Bioregion].push(d)
+	})
+
+	// chart 1 column chart
+	const fields = soefinding.findingJson.meta.fields.slice(-2) // last 2
+	// gather series
+	const qldTotal = {}
+	for (let bioregion in bioregions) {
+		soefinding.findingContent[bioregion] = {
+			series1: fields.map((f, i) => {
+				return {
+					name: f,
+					data: bioregions[bioregion].map(d => {
+
+						// as a side effect, populate the qld series
+						if (!qldTotal[d.name])
+							qldTotal[d.name] = [0, 0]
+						qldTotal[d.name][i] += d[f]
+
+						return d[f]
+					})
+				}
+			}),
+			groups: bioregions[bioregion].map(d => d.name)
+		}
+	}
+	soefinding.findingContent.Queensland = {
+		series1: fields.map((f, i) => {
+			return {
+				name: f,
+				data: Object.keys(qldTotal).map(q => qldTotal[q][i])
+
+			}
+		}),
+		groups: Object.keys(qldTotal)
+	}
+
 	const options1 = soefinding.getDefaultColumnChartOptions()
-	options1.xaxis.categories = fields
-	options1.xaxis.title = "Hecatares"
-	options1.yaxis.title = "Broad Vegetation Group"
-	options1.yaxis.labels.formatter = val => val >= 1000000 ? `${val / 1000000}M` : `${val / 1000}k`
 	options1.tooltip = { y: { formatter: val => val.toLocaleString() } }
-	
+	options1.xaxis.categories = soefinding.findingContent[soefinding.state.currentRegionName].groups
+	options1.xaxis.labels = {
+		trim: true,
+		hideOverlappingLabels: false
+	}
+	options1.xaxis.title = "Hectares"
+	options1.yaxis.labels.formatter = val => val >= 1000000 ? `${val / 1000000}M` : `${val / 1000}k`
+	options1.yaxis.title = "Broad Vegetation Group"
+
 	soefinding.state.chart1 = {
 		options: options1,
 		series: soefinding.findingContent[soefinding.state.currentRegionName].series1,
