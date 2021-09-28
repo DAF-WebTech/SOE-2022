0fc03b24b0aac2f8e12c0255dd3876053b0ef205
PORTER David <david.porter@daf.qld.gov.au>
2021-09-28 11:10:44 +1000

    finish chart 1

--- src/biodiversity/terrestrial-ecosystems/extent-change-remnant.js
+++ src/biodiversity/terrestrial-ecosystems/extent-change-remnant.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	var latestYear = years.at(-1)
 
 	// sort data by latest year
-	soefinding.findingJson.data.sort(function(a, b) {
+	soefinding.findingJson.data.sort(function (a, b) {
 		return b[latestYear] - a[latestYear]
 	})
 
@@ -23,18 +23,18 @@ document.addEventListener("DOMContentLoaded", function () {
 		}
 		bioregions[d.Bioregion].push(d)
 
-        let qldGroup = qldData.find(q => q["Broad vegetation group label"] == d["Broad vegetation group label"])
-        if (!qldGroup) {
-        	qldGroup = {
-        		"Broad vegetation group label":  d["Broad vegetation group label"],
-        	    [latestYear]: 0
-        	}
-        	qldData.push(qldGroup)
-        }
-        qldGroup[latestYear] += d[latestYear]
+		let qldGroup = qldData.find(q => q["Broad vegetation group label"] == d["Broad vegetation group label"])
+		if (!qldGroup) {
+			qldGroup = {
+				"Broad vegetation group label": d["Broad vegetation group label"],
+				[latestYear]: 0
+			}
+			qldData.push(qldGroup)
+		}
+		qldGroup[latestYear] += d[latestYear]
 	})
 
-    // set our chart data for each region
+	// set our chart data for each region
 	for (const bioregion in bioregions) {
 		soefinding.findingContent[bioregion] = {
 			app1: bioregions[bioregion].map((d, i) => {
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
 		}
 	}
 	// set chart date for qld, but first needs sorting
-	qldData.sort(function(a, b) {
+	qldData.sort(function (a, b) {
 		return b[latestYear] - a[latestYear]
 	})
 	soefinding.findingContent.Queensland = {
@@ -53,52 +53,58 @@ document.addEventListener("DOMContentLoaded", function () {
 		labels: qldData.map(d => d["Broad vegetation group label"]) // or could be keys of bioregions
 	}
 
-		const options1 = soefinding.getDefaultPieChartOptions();
-		options1.chart.id
-		options1.labels = soefinding.findingContent[soefinding.state.currentRegionName].labels
-		options1.tooltip = { y: {
+	const options1 = soefinding.getDefaultPieChartOptions();
+	options1.chart.id = "chart1"
+	options1.labels = soefinding.findingContent[soefinding.state.currentRegionName].labels
+	options1.tooltip = {
+		y: {
 			formatter: (val, options) => {
 				const percent = options.globals.seriesPercent[options.seriesIndex][0]
 				return `${val.toLocaleString()}ha (${percent.toFixed(1)}%)`
 			}
-		}}
-		options1.xaxis.categories = ["Broad vegetation group", "Hectares"] // these are the table headings
-
-
-		soefinding.state.chart1 = {
-			options: options1,
-			series: soefinding.findingContent[soefinding.state.currentRegionName].app1,
-			chartactive: true,
 		}
+	}
+	options1.xaxis.categories = ["Broad vegetation group", "Hectares"] // these are the table headings
 
 
-		new Vue({
-			el: "#chartContainer",
-			data: soefinding.state,
-			computed: {
-				heading1: () => `Proportion of broad vegetation groups in ${soefinding.state.currentRegionName}, ${latestYear}`,
-				heading2: () => "Percentage change in area between 1999 and 2019"
-			},
-			methods: {
-				formatter1: val => val.toLocaleString()
-			}
+	soefinding.state.chart1 = {
+		options: options1,
+		series: soefinding.findingContent[soefinding.state.currentRegionName].app1,
+		chartactive: true,
+	}
+
+
+	new Vue({
+		el: "#chartContainer",
+		data: soefinding.state,
+		computed: {
+			heading1: () => `Proportion of broad vegetation groups in ${soefinding.state.currentRegionName}, ${latestYear}`,
+			heading2: () => "Percentage change in area between 1999 and 2019"
+		},
+		methods: {
+			formatter1: val => val.toLocaleString()
+		}
+	})
+
+
+	window.soefinding.onRegionChange = function () {
+		// set the data series in each of the vue apps, for the current region
+
+		// chart 1
+		soefinding.state.chart1.series = this.findingContent[this.state.currentRegionName].app1
+		// this works on the table
+		soefinding.state.chart1.options.labels = this.findingContent[this.state.currentRegionName].labels
+		// but we also need this for the chart to update
+		ApexCharts.exec("chart1", "updateOptions", {
+			labels: this.findingContent[this.state.currentRegionName].labels,
 		})
 
 
-		window.soefinding.onRegionChange = function () {
-			// set the data series in each of the vue apps, for the current region
-			soefinding.state.chart1.series = this.findingContent[this.state.currentRegionName].app1
-			// this works on the table
-			soefinding.state.chart1.options.labels = this.findingContent[this.state.currentRegionName].labels
-			// but we also need this for the chart to update
-			ApexCharts.exec("chart1", "updateOptions", {
-				labels: this.findingContent[this.state.currentRegionName].labels 
-			})
 
 
 
-			soefinding.loadFindingHtml();
-		}
+		soefinding.loadFindingHtml();
+	}
 
 
-	})
+})
