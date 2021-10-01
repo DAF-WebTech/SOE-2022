8cb9fd45a10bd1b921fe707b391e7083fd2ed0e2
PORTER David <david.porter@daf.qld.gov.au>
2021-10-01 12:05:23 +1000

    fixes

--- src/biodiversity/estuarine-marine-ecosystems/condition-ecological-gbr.js
+++ src/biodiversity/estuarine-marine-ecosystems/condition-ecological-gbr.js
@@ -5,16 +5,16 @@ document.addEventListener("DOMContentLoaded", function () {
 	const series = soefinding.findingJson.data.map(d => {
 		return {
 			name: d["Ecological proceses"],
-			data: [data["Ecological processes condition statement"], data["Condition grade"]]
+			data: [d["Ecological processes condition statement"], d["Condition grade"]]
 		}
 	})
 
-	const options = { xaxis: { categories: soefinding.findingJson.meta.fields } }
+	const options = { xaxis: { categories: soefinding.findingJson.meta.fields.slice(1) } }
 
 	soefinding.state.chart1 = {
 		options: options,
-		series: series1,
-		chartactive: true,
+		series: series,
+		chartactive: false,
 	}
 
 
