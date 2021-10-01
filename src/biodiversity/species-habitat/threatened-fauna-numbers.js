5420e52dc92c16f6c5875202e5d584cd75a39f73
PORTER David <david.porter@daf.qld.gov.au>
2021-10-01 14:18:52 +1000

    initial commit

---
+++ src/biodiversity/species-habitat/threatened-fauna-numbers.js
@@ -0,0 +1,37 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+
+	const series1Keys = soefinding.findingJson.meta.fields.filter(f => f.includes("Threatened"))
+	const series1 = soefinding.findingJson.data.map(d => {
+		return {
+			name: d.Group,
+			data: series1Keys.map(k => d[k])
+		}
+	})
+
+	const options1 = soefinding.getDefaultStackedColumnChartOptions()
+	options1.xaxis.title.text = "Year"
+	options1.yaxis.title.text = "Number of species"
+
+	soefinding.state.chart1 = {
+		options: options1,
+		series: series1,
+		chartactive: true,
+	}
+
+
+
+
+	new Vue({
+		el: "#chartContainer",
+		data: soefinding.state,
+		computed: {
+			heading1: () => "Numbers of threatened fauna (<i>Nature Conservation Act 1992</i> Extinct, Extinct in the Wild, Critically Endangered, Endangered, and Vulnerable) by species group",
+			heading2: () => `Proportion of freshwater wetland systems extent in ${soefinding.state.currentRegionName}, 2024 TODO fix year`,
+		},
+		methods: {
+			formatter1: val => val.toLocaleString()
+		}
+	})
+})
