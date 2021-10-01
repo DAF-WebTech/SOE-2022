a4cd02a6e52a9ab2d210f1ff7393eea93e87e2d2
PORTER David <david.porter@daf.qld.gov.au>
2021-10-01 14:06:54 +1000

    simplify to straightforward vue

--- src/biodiversity/estuarine-marine-ecosystems/key-fish-stocks.js
+++ src/biodiversity/estuarine-marine-ecosystems/key-fish-stocks.js
@@ -2,36 +2,14 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-	const lastKey = soefinding.findingJson.meta.fields.at(-1)
-	const groups = {}
+	//get the json parsed data  
+	var jsonDataElement = document.getElementById("jsonData");
+	const parsed = JSON.parse(document.getElementById("jsonData").textContent)
+	console.log(parsed);
 
-	soefinding.findingJson.forEach(d => {
-		if (!groups[d.Group])
-			groups[d.Group] = { species: {} }
-
-		if (!groups[d.Group].species[d.Species])
-			groups[d.Group].species(d.Species) = []
-
-		groups[d.Group].species(d.Species).push(d)
+	new Vue({
+		el: "#chartContainer",
+		data: parsed,
 	})
-
-	const innerhtml = ""
-
-	Object.keys(groups).forEach((g) => {
-		const group = groups[g]
-		innerhtml += `<tr><td rowspan=${group.species).reduce((acc, cur) => { return acc + cur.length }, 0)
-} > ${g}` 
-
-	Object.keys(g.species).forEach((s, i) => {
-		const species = g.species[s]
-		if (i == 0)
-			innerhtml += `< tr > <td rowspan=${species.length}>s`
-		
-		innerhtml += species.map(s => `<td>${s.Stock}<td>${s["Stock status " + lastKey]}`)
-	})
-	
-})
-
-document.querySelector("div#chartContainer tbody").innerHtml = innerhtml
 
 })
