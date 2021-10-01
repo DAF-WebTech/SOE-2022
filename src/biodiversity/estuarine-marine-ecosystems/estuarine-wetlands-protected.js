8f052ac3b07c828fa6b0ac815d4745d486b6d29c
PORTER David <david.porter@daf.qld.gov.au>
2021-10-01 11:12:32 +1000

    add second chart

--- src/biodiversity/estuarine-marine-ecosystems/estuarine-wetlands-protected.js
+++ src/biodiversity/estuarine-marine-ecosystems/estuarine-wetlands-protected.js
@@ -2,14 +2,25 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-	const series1 = soefinding.findingJson.data[1].slice(1)
+	var series1Keys = ["National park (scientific)", "National park", "National park (Cape York Aboriginal land)", 
+	"Conservation park", "Resources reserve", "Nature refuge", 
+	"Marine conservation park zone", "Marine national park zone", "Marine preservation zone", 
+	"Buffer zone", "Scientific research zone", "Fish habitat area management level A", 
+	"Fish habitat area management level B"]
+
+
+	const series1 = [{ 
+		name: "Hectares",
+	 	data: series1Keys.map(k => soefinding.findingJson.data[0][k])
+	 }]
 
 	const options1 = soefinding.getDefaultColumnChartOptions()
 	options1.tooltip = { y: { formatter: val => val.toLocaleString() } }
-	options1.xaxis.categories = soefinding.findingJson.data[0].slice(1)
-	options1.xaxis.title = "Protected area type"
-	options1.yaxis.title = "Hectares"
-	options1.yaxis.labels.formatter = `${val / 1000}K`
+	options1.xaxis.categories = series1Keys
+	options1.xaxis.labels = { trim: true, maxHeight: 160 }
+	options1.xaxis.title.text = "Protected area type"
+	options1.yaxis.title.text = "Hectares"
+	options1.yaxis.labels.formatter = val => `${val / 1000}K`
 
 	soefinding.state.chart1 = {
 		options: options1,
@@ -18,15 +29,35 @@ document.addEventListener("DOMContentLoaded", function () {
 	}
 
 
+	const series2Keys = ["Total all protection", "Outside all protection"]
+	const series2 = series2Keys.map(k => soefinding.findingJson.data[0][k])
+
+	const options2 = soefinding.getDefaultPieChartOptions()
+	options2.labels = series2Keys
+	options2.tooltip = { y: { formatter: (val, options) => {
+		const percent = options.globals.seriesPercent[options.seriesIndex][0]
+		return `${val.toLocaleString()}ha (${percent.toFixed(1)}%)`
+	}}}
+	options2.xaxis.categories = ["Protection", "Hectares"]
+
+	soefinding.state.chart2 = {
+		options: options2,
+		series: series2,
+		chartactive: true,
+	}
+
+
+
 
 	new Vue({
 		el: "#chartContainer",
 		data: soefinding.state,
 		computed: {
-			heading1: () => "Proportion of estuarine wetlands within protected areas, 2024 TOD fix year",
+			heading1: () => "Proportion of estuarine wetlands within protected areas, 2024 TODO fix year",
 			heading2: () => "Overall protection of estuarine wetlands, 2024 TODO fix year",
 		},
 		methods: {
 			formatter1: val => val.toLocaleString(),
 		}
 	})
+})
