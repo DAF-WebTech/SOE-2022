fb0928366983b677af903ce2443c7d445dfa5c35
PORTER David <david.porter@daf.qld.gov.au>
2021-10-05 10:19:18 +1000

    add checkboxes and charts

--- src/biodiversity/species-habitat/threatened-fauna-numbers.js
+++ src/biodiversity/species-habitat/threatened-fauna-numbers.js
@@ -2,7 +2,8 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-	const series1Keys = soefinding.findingJson.meta.fields.filter(f => f.includes("threatened") && !f.includes("near"))
+	const series1Keys = soefinding.findingJson.meta.fields.filter(f => f.includes("threatened") 
+			&& !f.includes("near"))
 	const series1 = soefinding.findingJson.data.map(d => {
 		return {
 			name: d.Group,
@@ -21,6 +22,45 @@ document.addEventListener("DOMContentLoaded", function () {
 		chartactive: true,
 	}
 
+	// now set up check box list 
+	// and a chart component for each item in data
+	const years = options1.xaxis.categories
+	// there's a bad situation here where they changed the name of extinct in 2019, we need to normalise this.
+	soefinding.findingJson.data.forEach(d => {
+		years.forEach(y => {
+			if (d.hasOwnProperty(`${y} extinct in the wild`))
+				d[`${y} presumed extinct`] = d[`${y} extinct in the wild`]
+		})
+	})
+
+
+	const series2Fields = ["vulnerable", "endangered", "presumed extinct"]
+	const series2Keys = []
+	years.forEach(y => {
+		series2Fields.forEach(f => series2Keys.push(`${y} ${f}`))
+	})
+
+	const species = soefinding.findingJson.data.map(d => {
+		return { 
+			name: d.Group, 
+			checked: false,
+			chartactive: true,
+			data: series2Fields.map(f => { 
+				return {
+					name: f[0].toUpperCase() + f.slice(1), 
+					data: series2Keys.filter(k => k.includes(f)).map(k => d[k])
+				}
+			}),
+		}
+	})
+	species[0].checked = true
+	soefinding.state.species = species
+
+	soefinding.state.options2 = soefinding.getDefaultStackedColumnChartOptions()
+	soefinding.state.options2.xaxis.categories = options1.xaxis.categories
+	soefinding.state.options2.xaxis.title.text = "Year"
+	soefinding.state.options2.yaxis.title.text = "Number of species"
+
 
 
 
@@ -28,11 +68,10 @@ document.addEventListener("DOMContentLoaded", function () {
 		el: "#chartContainer",
 		data: soefinding.state,
 		computed: {
-			heading1: () => "Numbers of threatened fauna (<i>Nature Conservation Act 1992</i> Extinct, Extinct in the Wild, Critically Endangered, Endangered, and Vulnerable) by species group",
-			heading2: () => `Proportion of freshwater wetland systems extent in ${soefinding.state.currentRegionName}, 2024 TODO fix year`,
+			heading1: () => "Numbers of threatened fauna (<i>Nature Conservation Act 1992</i> Extinct, Extinct in the Wild, Critically Endangered, Endangered, and Vulnerable) by species group"
 		},
 		methods: {
-			formatter1: val => val.toLocaleString()
+			formatter1: val => val
 		}
 	})
 })
