53eae2931bf57b541cdaf310092b42c1408283b8
David Porter <david.porter@fastmail.com>
2021-10-11 03:10:39 +0000

    change to datetime series

--- src/pollution/air-quality/lead-concentrations.js
+++ src/pollution/air-quality/lead-concentrations.js
@@ -6,27 +6,50 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	// chart 1. lead particles
 	const coSeries = soefinding.fields.map(f => {
-	    return { name: f,
-	             data: soefinding.findingJson.data.map(d => d[f])
-	            } 
+		return {
+			name: f,
+			data: soefinding.findingJson.data.map(d => {
+				return {
+					x: `${d.Year}-${String(d.Month).padStart(2, "0")}`,
+					y: d[f]
+				}
+			})
+		}
 	})
 
 	// create the vue instance for first chart, our column chart
-	const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 	const options1 = soefinding.getDefaultLineChartOptions()
+
 	delete options1.markers
-	options1.xaxis.categories = soefinding.findingJson.data.map(d => `${months[d.Month]} ${d.Year}`)
-	options1.xaxis.title.text = "Month/Year"
-	options1.yaxis.title.text = "Lead concentrations (µg/m³)"
+	delete options1.xaxis.categories
+
+	options1.stroke = { width: 2 }
+
+	const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+	options1.tooltip.x = {
+		formatter: function (val) {
+			const d = new Date(val)
+			return `${months[d.getMonth()]} ${d.getFullYear()}`
+		}
+	}
 	options1.tooltip.y = {
 		formatter: function (val) {
-		    return val ? `${val} µg/m³` : "—"
-	    }
+			return val ? `${val} µg/m³` : "—"
+		}
 	}
-	options1.yaxis.labels.formatter = function (val) {
-		return val
-	}
-	
+
+	options1.xaxis.type = "datetime"
+	options1.xaxis.title.text = "Month/Year"
+
+	options1.yaxis.title.text = "Lead concentrations (µg/m³)"
+	options1.yaxis.forceNiceScale = false
+	options1.yaxis.min = 0
+	options1.yaxis.max = 3
+	options1.yaxis.tickAmount = 6
+	options1.yaxis.labels.formatter = val => val.toFixed(1) 
+
+
+
 
 	soefinding.state.chart1 = {
 		options: options1,
@@ -47,4 +70,3 @@ document.addEventListener("DOMContentLoaded", function () {
 		}
 	})
 })
-
