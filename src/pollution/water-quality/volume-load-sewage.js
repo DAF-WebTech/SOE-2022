303fae04f8f2a0468aff910a7ef08fdfefbdfd12
PORTER David <david.porter@daf.qld.gov.au>
2021-10-13 14:51:35 +1000

    regional chart 2

--- src/pollution/water-quality/volume-load-sewage.js
+++ src/pollution/water-quality/volume-load-sewage.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	const years = soefinding.findingJson.meta.fields.slice(2)
 
+	soefinding.findingContent.Queensland
+
 	const series1Keys = ["Total Nitrogen (tonne)", "Total Phosphorus (tonne)"]
 	const series1 = series1Keys.map(k => {
 		return {
@@ -13,44 +15,59 @@ document.addEventListener("DOMContentLoaded", function () {
 			}, 0))
 		}
 	})
+	soefinding.findingContent.Queensland = { series1 }
+
+	const regions = ["South East Queensland", "Great Barrier Reef"]
+	regions.forEach(r => {
+		const series1Items = soefinding.findingJson.data.filter(d => d.Region == r && d.Variable != "Annual volume (GL)")
+		const regionSeries1 = series1Items.map(d => {
+			return {
+				name: d.Variable,
+				data: years.map(y => d[y])
+			}
+		})
+		soefinding.findingContent[r] = { series1: regionSeries1 }
+	})
 
 	const options1 = soefinding.getDefaultColumnChartOptions()
+	options1.chart.id = "chart1"
 	options1.xaxis.categories = years
 	options1.xaxis.title.text = "Year"
 	options1.yaxis.title.text = "Load (tonnes)"
 	options1.yaxis.labels.formatter = val => val.toLocaleString()
-	options1.yaxis.forceNiceScale = false
-	options1.yaxis.tickAmount = 6
-	options1.yaxis.min = 0
-	options1.yaxis.max = 1500
+
 
 	soefinding.state.chart1 = {
 		options: options1,
-		series: series1,
+		series: soefinding.findingContent[soefinding.state.currentRegionName].series1,
 		chartactive: true,
 	}
 
 
-	const series2Items = soefinding.findingJson.data.filter(d => d.Variable == "Annual volume (GL)")
-	const series2 = series2Items.map(d => {
-		return {
-			name: d.Region,
-			data: years.map(y => d[y])
+	soefinding.findingContent.Queensland.series2 = []
+	regions.forEach(r => {
+
+		const series2Item = soefinding.findingJson.data.find(d => d.Region == r && d.Variable == "Annual volume (GL)")
+		const item = {
+			name: r,
+			data: years.map(y => series2Item[y])
 		}
+
+		soefinding.findingContent.Queensland.series2.push(item)
+
+		soefinding.findingContent[r].series2 = [item]
 	})
 
 	const options2 = soefinding.getDefaultLineChartOptions()
+	options2.chart.id = "chart2"
 	options2.xaxis.categories = years
 	options2.xaxis.title.text = "Year"
 	options2.yaxis.title.text = "Annual Volume (gigalitres)"
-	options2.yaxis.forceNiceScale = false
-	options2.yaxis.tickAmount = 6
-	options2.yaxis.min = 0
-	options2.yaxis.max = 300
+
 
 	soefinding.state.chart2 = {
 		options: options2,
-		series: series2,
+		series: soefinding.findingContent[soefinding.state.currentRegionName].series2,
 		chartactive: true,
 	}
 
@@ -60,8 +77,22 @@ document.addEventListener("DOMContentLoaded", function () {
 		el: "#chartContainer",
 		data: soefinding.state,
 		computed: {
-			heading1: () => "Treated sewage total nitrogen and phosphorous (for SEQ and GBR)",
-			heading2: () => "Trends in sewage load volumes, by region"
+			heading1: function () {
+				let retVal = "Treated sewage total nitrogen and phosphorous"
+				if (this.currentRegionName == "Queensland")
+					retVal += " (for SEQ and GBR)"
+				else
+					retVal += " in " + this.currentRegionName
+				return retVal
+			},
+			heading2: function () {
+				let retVal = "Trends in sewage load"
+				if (this.currentRegionName == "Queensland")
+					retVal += " volumes, by region"
+				else
+					retVal += " volume in " + this.currentRegionName
+				return retVal
+			}
 		},
 		methods: {
 			formatter1: val => val.toLocaleString()
@@ -73,19 +104,12 @@ document.addEventListener("DOMContentLoaded", function () {
 		// set the data series in each of the vue apps, for the current region
 
 		// chart 1
-		// ApexCharts.exec("chart1", "updateSeries", this.findingContent[this.state.currentRegionName].series1)
-		// soefinding.state.chart1.series = this.findingContent[this.state.currentRegionName].series1
+		ApexCharts.exec("chart1", "updateSeries", this.findingContent[this.state.currentRegionName].series1)
+		soefinding.state.chart1.series = this.findingContent[this.state.currentRegionName].series1
 
-		// // but we also need this for the chart to update
-		// ApexCharts.exec("chart1", "updateOptions", {
-		// 	xaxis: { categories: this.findingContent[this.state.currentRegionName].groups }
-		// }, true)
-		// // this works on the table
-		// options1.xaxis.categories = this.findingContent[this.state.currentRegionName].groups
-
-		// // chart 2, pie chart, labels stay the same
-		// ApexCharts.exec("chart2", "updateSeries", this.findingContent[this.state.currentRegionName].series2)
-		// soefinding.state.chart2.series = this.findingContent[this.state.currentRegionName].series2
+		// chart 1
+		ApexCharts.exec("chart2", "updateSeries", this.findingContent[this.state.currentRegionName].series2)
+		soefinding.state.chart2.series = this.findingContent[this.state.currentRegionName].series2
 
 
 		soefinding.loadFindingHtml()
