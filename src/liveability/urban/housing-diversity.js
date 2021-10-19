9f3df78c6693ef081258b8ccf473c644f75d8ed5
PORTER David <david.porter@daf.qld.gov.au>
2021-10-19 15:56:07 +1000

    add chart 1

--- src/liveability/urban/housing-diversity.js
+++ src/liveability/urban/housing-diversity.js
@@ -2,6 +2,25 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
+	const keys = soefinding.findingJson.meta.fields.slice(2, 5)
+	soefinding.state.year = "2016"
+
+
+	const series1Items = soefinding.findingJson.data.filter(d=> d.Measure == "2016 Census Number of Dwellings")
+	series1Items.forEach(d => 
+		soefinding.findingContent[d["Regional Planning Area"]] = { series1: keys.map(k => d[k]) }
+	)
+
+	const options1 = soefinding.getDefaultPieChartOptions()
+	options1.labels = keys.map(k => k.replace("-", ""))
+	options1.xaxis.categories = ["Type", "Amount"]
+
+	soefinding.state.chart1 = {
+		series: soefinding.findingContent[soefinding.state.currentRegionName].series1,
+		options: options1,
+		chartactive: true,
+	}
+
 
 
 
@@ -9,7 +28,12 @@ document.addEventListener("DOMContentLoaded", function () {
 		el: "#chartContainer",
 		data: soefinding.state,
 		computed: {
-			heading1: () => "",
+			heading1: function() { 
+				if (this.currentRegionName == "Queensland")
+					return `Number of dwellings in ${this.year} Queensland census`
+				else
+					return `Number of dwellings in ${this.currentRegionName} in ${this.year} Queensland census`
+			},
 			heading2: () => "",
 			heading3: () => "",
 			heading4: () => "",
