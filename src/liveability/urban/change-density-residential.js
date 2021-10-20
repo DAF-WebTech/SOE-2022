272b4708e2c6f76f181dbb5f7dd187f84d243eca
PORTER David <david.porter@daf.qld.gov.au>
2021-10-20 15:52:23 +1000

    fix chart 1

--- src/liveability/urban/change-density-residential.js
+++ src/liveability/urban/change-density-residential.js
@@ -1,7 +1,6 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-
 	const densityKeys = soefinding.findingJson.meta.fields.slice(2, 4)
 
 	const series1Items = soefinding.findingJson.data.filter(d => d.Measure == "Dwelling density" && d["Regional Planning Area"] != "Queensland")
@@ -13,9 +12,15 @@ document.addEventListener("DOMContentLoaded", function () {
 	})
 
 	const options1 = soefinding.getDefaultColumnChartOptions()
-	options1.xaxis.categories = series1Items.map(d => d["Regional Planning Area"])
+	options1.xaxis.categories = series1Items.map(d => d["Regional Planning Area"].split(/\s|–/))
+	options1.xaxis.labels.hideOverlappingLabels = false
+	options1.xaxis.labels.rotate = 0
+	options1.xaxis.labels.rotateAlways = false
+	options1.xaxis.tickPlacement = "between"
 	options1.xaxis.title.text = "Region planning area"
 	options1.yaxis.title.text = "Dwellings/hectare"
+	options1.yaxis.tickAmount = 4
+	options1.yaxis.labels.formatter = val => Math.round(val)
 
 	soefinding.state.chart1 = {
 		series: series1,
@@ -36,14 +41,17 @@ document.addEventListener("DOMContentLoaded", function () {
 			heading4: () => "",
 		},
 		methods: {
-			formatter1: val => val.toLocaleString(),
+			formatter1: val => val.toFixed(1),
 		}
 	})
 
+	
 	window.soefinding.onRegionChange = function () {
+		
 
 
 		soefinding.loadFindingHtml()
-	})
+	}
+
 
 })
