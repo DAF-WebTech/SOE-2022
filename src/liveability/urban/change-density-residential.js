4eb27deedf466ecea3b80f29fd26e0c0a997aa43
PORTER David <david.porter@daf.qld.gov.au>
2021-10-21 08:24:16 +1000

    add chart 2

--- src/liveability/urban/change-density-residential.js
+++ src/liveability/urban/change-density-residential.js
@@ -3,6 +3,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const densityKeys = soefinding.findingJson.meta.fields.slice(2, 4)
 
+	// line chart for qld only, dwelling density
 	const series1Items = soefinding.findingJson.data.filter(d => d.Measure == "Dwelling density" && d["Regional Planning Area"] != "Queensland")
 	const series1 = densityKeys.map(k => {
 		return {
@@ -21,12 +22,38 @@ document.addEventListener("DOMContentLoaded", function () {
 	options1.yaxis.title.text = "Dwellings/hectare"
 	options1.yaxis.tickAmount = 4
 	options1.yaxis.labels.formatter = val => Math.round(val)
+	options1.tooltip.y = { formatter: val => val }
 
 	soefinding.state.chart1 = {
 		series: series1,
 		options: options1,
 		chartactive: true,
 	}
+	soefinding.findingContent.Queensland = { html: "" }
+
+
+	// line chart for each region, dwelling density
+	series1Items.forEach(d => {
+		soefinding.findingContent[d["Regional Planning Area"]] = {
+			series2: [{
+				name: "Density",
+				data: densityKeys.map(k => d[k])
+			}]
+		}
+	})
+
+	const options2 = soefinding.getDefaultColumnChartOptions()
+	options2.xaxis.categories = densityKeys
+	options2.xaxis.tickPlacement = "between"
+	options2.xaxis.title.text = "Year"
+	options2.yaxis.title.text = "Dwellings/hectare"
+
+	soefinding.state.chart2 = {
+		series: soefinding.findingContent[soefinding.state.currentRegionName].series2,
+		options: options2,
+		chartactive: true,
+	}
+
 
 
 
@@ -36,6 +63,7 @@ document.addEventListener("DOMContentLoaded", function () {
 		computed: {
 			heading1: () => "Mean population-weighted dwelling density for Queensland",
 			heading2: function() {
+				return `Mean population-weighted dwelling density for ${this.currentRegionName}`
 			},
 			heading3: () => "",
 			heading4: () => "",
@@ -47,7 +75,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	
 	window.soefinding.onRegionChange = function () {
-		
+		soefinding.state.chart2.series = soefinding.findingContent[soefinding.state.currentRegionName].series2
 
 
 		soefinding.loadFindingHtml()
