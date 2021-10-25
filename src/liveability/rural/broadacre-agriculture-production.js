7f4a4059bf8f69d748316b89a0c85d4af1ae5449
PORTER David <david.porter@daf.qld.gov.au>
2021-10-25 12:27:33 +1000

    add chart 2

--- src/liveability/rural/broadacre-agriculture-production.js
+++ src/liveability/rural/broadacre-agriculture-production.js
@@ -32,6 +32,9 @@ document.addEventListener("DOMContentLoaded", function () {
 	const year_keys = [4, 5, 6]
 	const years = year_keys.map(yk => header[yk])
 
+	const year_keys_value = [8, 9, 10]
+	const years_value = year_keys_value.map(yk => header[yk])
+
 
 	// group by region and subregion
 	soefinding.findingContent = {}
@@ -54,6 +57,15 @@ document.addEventListener("DOMContentLoaded", function () {
 				chart2: {}
 			}
 		}
+
+		// fix the data, replace n.p. with null
+		d.forEach(function(item, i) { 
+			if (item == "n.p.") 
+				d[i] = null
+			else if (String(item).indexOf("&") >= 0) 
+				d[i] = item.replace("&", "and")
+		})
+
 		if (d[product_key] != "Total")
 			soefinding.state.regionData[d[region_key]][d[subregion_key]].data.push(d)
 	})
@@ -64,6 +76,26 @@ document.addEventListener("DOMContentLoaded", function () {
 	soefinding.state.columnChartOptions.yaxis.title.text = "Tonnes"
 	soefinding.state.columnChartOptions.yaxis.labels.formatter = val => val >= 1000000 ? `${val/1000000}M` : ( val >= 1000 ? `${val/1000}K`: val)
 	soefinding.state.columnChartOptions.tooltip.y = { formatter: val => val.toLocaleString() }
+
+	soefinding.state.lineChartOptions = soefinding.getDefaultLineChartOptions()
+	soefinding.state.lineChartOptions.xaxis.categories = years_value.map(k => k.replace("-", "–")) // ndash
+	soefinding.state.lineChartOptions.xaxis.title.text = "Year"
+	soefinding.state.lineChartOptions.xaxis.tickPlacement = "between"
+	soefinding.state.lineChartOptions.tooltip.y = { formatter: val => val?.toLocaleString() ?? "n.p." }
+	soefinding.state.lineChartOptions.yaxis.title.text = "Tonnes"
+	soefinding.state.lineChartOptions.yaxis.labels.minWidth = 30
+	soefinding.state.lineChartOptions.yaxis.labels.formatter = val => {
+		if (val >= 1000000000)
+			return `${val/1000000000}B`
+		else if (val >= 1000000)
+			return `${val/1000000}M`
+		else if (val >= 1000)
+			return `${val/1000}K`
+		else 
+			return val
+	}
+
+
 	
 	Object.keys(soefinding.state.regionData).forEach(function(regionName) {
 		Object.keys(soefinding.state.regionData[regionName]).forEach(function(subregionName) {
@@ -73,7 +105,7 @@ document.addEventListener("DOMContentLoaded", function () {
 				if (regionName != "Queensland")
 					 heading += "NRM region"
 				if (regionName != subregionName)
-					heading += ` — ${subregionName}`
+					heading += ` — ${subregionName}` // mdash
 
 				const zeroSeries = year_keys.reduce( function (acc, curr) {
 							return acc + d[curr]
@@ -88,36 +120,36 @@ document.addEventListener("DOMContentLoaded", function () {
 						zeroSeries
 				}
 			})
-			soefinding.state.regionData[regionName][subregionName].chart2 = {} //line chart for production values TODO
+
+			// put together chart 2, one line chart per subregion
+			let heading2 = `Production values in ${regionName} `
+			if (regionName != "Queensland")
+				heading2 += "NRM region"
+			if (regionName != subregionName)
+				heading2 += ` — ${subregionName}`  // mdash
+
+			soefinding.state.regionData[regionName][subregionName].chart2 = {
+				heading2, 
+				series: soefinding.state.regionData[regionName][subregionName].data.map(d => {
+					return {
+						name: d[product_key],
+						data: year_keys_value.map(ykv => d[ykv])
+					}
+				}),
+				chartactive: true
+			}
 		})
 	})
-
-
-
-
-
-
 
 
 	new Vue({
 		el: "#chartContainer",
 		data: soefinding.state,
-		computed: {
-			heading1: function () {
-				return `Production amount of XXX in YYY`
-			}
-
-		},
-		methods: {
-			formatter1: val => val?.toLocaleString(2) ?? "0"
-		}
 	})
-
 
 
 	window.soefinding.onRegionChange = function () {
 		soefinding.loadFindingHtml()
 	}
-
 
 })
