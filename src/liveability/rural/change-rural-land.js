caf0564e77bb9299cebb606348b072aae33edb28
PORTER David <david.porter@daf.qld.gov.au>
2021-10-26 11:32:38 +1000

    add chart 2

--- src/liveability/rural/change-rural-land.js
+++ src/liveability/rural/change-rural-land.js
@@ -16,9 +16,11 @@ document.addEventListener("DOMContentLoaded", function () {
 		regions.get(d.Region).push(d)
 	})
 
-	// create series 1 for each region
+	// create series for each region
 	for(let [region, data] of regions) {
 		soefinding.findingContent[region] = {}
+
+		// chart 1, a stacked column chart, same for qld and each region
 		soefinding.findingContent[region].series1 = data.filter(d => d.Use != "total area mapped").map(d => {
 			let firstIndex = -1, lastIndex = 0
 			yearKeys.forEach((y, i) => {
@@ -34,6 +36,15 @@ document.addEventListener("DOMContentLoaded", function () {
 				data: soefinding.findingContent[region].series1categories.map(c => d[c])
 			}
 		})
+
+		// chart 2, a pie chart, same for qld and each region
+		soefinding.findingContent[region].series2LatestYear = soefinding.findingContent[region].series1categories[1]
+		soefinding.findingContent[region].series2 = data.filter(d => d.Use != "total area mapped").map(d => d[soefinding.findingContent[region].series2LatestYear])
+		const totalRural = soefinding.findingContent[region].series2.reduce((acc, curr) => acc+curr)
+		const totalMapped = data.find(d => d.Use == "total area mapped")["total area mapped"]
+		const nonRural = totalMapped - totalRural
+		soefinding.findingContent[region].series2.push(nonRural)
+
 	}
 
 	const options1 = soefinding.getDefaultStackedColumnChartOptions()
@@ -49,7 +60,22 @@ document.addEventListener("DOMContentLoaded", function () {
 		series: soefinding.findingContent[soefinding.state.currentRegionName].series1,
 		options: options1,
 		chartactive: true,
-	};
+	}
+
+
+	const options2 = soefinding.getDefaultPieChartOptions()
+	options2.labels = ["Rural Land in Intensive Use", "Rural Land in Extensive Use", "Rural Land Not Settled", "Non Rural area"]
+	options2.xaxis.categories = ["Use", soefinding.findingContent[soefinding.state.currentRegionName].seriesLatestYear]
+	options2.tooltip = { y: { formatter: (val, options) => {
+		const percent = options.globals.seriesPercent[options.seriesIndex][0]
+		return `${val.toLocaleString()} (${percent.toFixed(1)}%)`
+	}}}
+
+	soefinding.state.chart2 = {
+		series: soefinding.findingContent[soefinding.state.currentRegionName].series2,
+		options: options2,
+		chartactive: true,
+	}
 
 
 	new Vue({
@@ -60,6 +86,16 @@ document.addEventListener("DOMContentLoaded", function () {
 				let retVal = `Rural area growth between ${soefinding.findingContent[this.currentRegionName].series1categories[0]} and ${soefinding.findingContent[this.currentRegionName].series1categories[1]}`
 				if (this.currentRegionName == "Queensland")
 					retVal += "*"
+				else
+					retVal += ` in ${this.currentRegionName}`
+				return retVal
+			},
+			heading2: function() {
+				let retVal = `Proportion of rural and other areas as at ${soefinding.findingContent[this.currentRegionName].series2LatestYear}`
+				if (this.currentRegionName == "Queensland")
+					retVal += "*"
+				else
+					retVal += ` in ${this.currentRegionName}`
 				return retVal
 			}
 		},
@@ -75,6 +111,12 @@ document.addEventListener("DOMContentLoaded", function () {
 			xaxis: { categories: soefinding.state.chart1.options.xaxis.categories }
 		}, true)
 		soefinding.state.chart1.series = soefinding.findingContent[soefinding.state.currentRegionName].series1
+
+
+
+		soefinding.state.chart2.options.xaxis.categories[1] = soefinding.findingContent[soefinding.state.currentRegionName].series2LatestYear
+		soefinding.state.chart2.series = soefinding.findingContent[soefinding.state.currentRegionName].series2
+
 
 		soefinding.loadFindingHtml()
 	}
