b66d97724ccc3f98b0fd8330e34e47efa8f0dd02
PORTER David <david.porter@daf.qld.gov.au>
2021-10-26 15:19:08 +1000

    fixes

--- src/heritage/aboriginal-torres-strait/places.js
+++ src/heritage/aboriginal-torres-strait/places.js
@@ -5,11 +5,13 @@ document.addEventListener("DOMContentLoaded", function () {
 	const years = soefinding.findingJson.meta.fields.slice(1)
 
 	soefinding.findingContent.Queensland = { series: [] }
-	soefinding.findingJson.forEach(d => {
-		soefinding.findingContent[d.Region].series = [{
-			name: "Places",
-			data: years.map(y => d[y])
-		}]
+	soefinding.findingJson.data.forEach(d => {
+		soefinding.findingContent[d.Region] = { 
+			series: [{
+				name: "Places",
+				data: years.map(y => d[y])
+			}]
+		}
 
 		soefinding.findingContent.Queensland.series.push({
 			name: d.Region,
@@ -22,11 +24,15 @@ document.addEventListener("DOMContentLoaded", function () {
 	options1.xaxis.title.text = "Year"
 	options1.xaxis.categories = years
 	options1.yaxis.title.text = "Number of places"
+	options1.yaxis.forceNiceScale = false
+	options1.yaxis.max = 10
+	options1.yaxis.min = 0
+	options1.yaxis.tickAmount = 10
 
 
 
 	soefinding.state.chart1 = {
-		series: soefindingContent[soefinding.state.currentRegionName].series,
+		series: soefinding.findingContent[soefinding.state.currentRegionName].series,
 		options: options1,
 		chartactive: true,
 	}
@@ -50,7 +56,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 	window.soefinding.onRegionChange = function () {
-		soefinding.state.chart1.series = soefinding.findingContent[soefinding.state.currentRegionName].series1
+		soefinding.state.chart1.series = soefinding.findingContent[soefinding.state.currentRegionName].series
 
 		soefinding.loadFindingHtml()
 	}
