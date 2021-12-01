ec89d9efc8517db4ef89de7ada2eb0458657fbce
David Porter <david.porter@daf.qld.gov.au>
2021-12-02 08:48:00 +1000

    initial commit

---
+++ src/heritage/historic/local-heritage-places.js
@@ -0,0 +1,35 @@
+"use strict"
+document.addEventListener("DOMContentLoaded", function () {
+
+	const localPlaces = soefinding.finding.Json.data.filter(d => d["Local heritage places identified in a local heritage register"] > 0)
+	const series1 = [{
+		name: "Places",
+		data: localPlaces.map(p => p["Local heritage places identified in a local heritage register"])
+	}]
+
+	const options1 = soefinding.getDefaultPieChartOptions()
+	options1.labels = localPlaces.map(p => p.LGA)
+	options1.tooltip = { y: { formatter: (val, options) => {
+		const percent = options.globals.seriesPercent[options.seriesIndex][0]
+		return `${val.toLocaleString()}ha (${percent.toFixed(1)}%)`
+	}}}
+
+	soefinding.state.chart1 = {
+		series: series1,
+		options: options1,
+		chartactive: true,
+	}
+
+	new Vue({
+		el: "#chartContainer",
+		data: soefinding.state,
+		computed: {
+			heading1: () => "Proportion of local heritage places on local heritage registers by local government area (LGA), 2020 (TODO fix year)"
+		},
+		methods: {
+			formatter1: val => val.toLocaleString(),
+		}
+	})
+
+
+})
