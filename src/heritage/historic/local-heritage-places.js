984144af598773e9de489f0d52b98102e95d70ff
David Porter <david.porter@daf.qld.gov.au>
2021-12-02 09:06:27 +1000

    add pie chart

--- src/heritage/historic/local-heritage-places.js
+++ src/heritage/historic/local-heritage-places.js
@@ -1,11 +1,13 @@
 "use strict"
 document.addEventListener("DOMContentLoaded", function () {
 
-	const localPlaces = soefinding.finding.Json.data.filter(d => d["Local heritage places identified in a local heritage register"] > 0)
-	const series1 = [{
-		name: "Places",
-		data: localPlaces.map(p => p["Local heritage places identified in a local heritage register"])
-	}]
+	const LOCAL = "Local heritage places identified in a local heritage register"
+	const localPlaces = soefinding.findingJson.data.filter(d => d[LOCAL] > 0)
+	localPlaces.sort(function(a, b) {
+		return b[LOCAL] - a[LOCAL]
+	})
+	
+	const series1 = localPlaces.map(p => p[LOCAL])
 
 	const options1 = soefinding.getDefaultPieChartOptions()
 	options1.labels = localPlaces.map(p => p.LGA)
@@ -13,6 +15,8 @@ document.addEventListener("DOMContentLoaded", function () {
 		const percent = options.globals.seriesPercent[options.seriesIndex][0]
 		return `${val.toLocaleString()} (${percent.toFixed(1)}%)`
 	}}}
+	options1.xaxis.categories = ["LGA", LOCAL.replace("identified", "identified<br>")
+	]
 
 	soefinding.state.chart1 = {
 		series: series1,
@@ -24,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
 		el: "#chartContainer",
 		data: soefinding.state,
 		computed: {
-			heading1: () => "Proportion of local heritage places on local heritage registers by local government area (LGA), 2020 (TODO fix year)"
+			heading1: () => "Proportion of local heritage places on local heritage registers by local government area, 2020 (TODO fix year)"
 		},
 		methods: {
 			formatter1: val => val.toLocaleString(),
