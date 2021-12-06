6f57828e2b828e87b9c13601e3040a1745e8d0df
PORTER David <david.porter@daf.qld.gov.au>
2021-12-06 11:56:32 +1000

    add subcatchments

--- src/biodiversity/terrestrial-ecosystems/extent-change-riparian.js
+++ src/biodiversity/terrestrial-ecosystems/extent-change-riparian.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	const catchments = soefinding.findingJson.data.filter(d => d.Catchment == d.Subcatchment)
 	const series1Keys = soefinding.findingJson.meta.fields.filter(f => f.includes("Woody Vegetation Loss per cent"))
-	soefinding.findingContent.Queensland = { series1: null}
+	soefinding.findingContent.Queensland = { series1: null, subcatchments: null}
 
 	catchments.forEach(d => {
 		soefinding.findingContent[d.Catchment] = { 
@@ -13,6 +13,18 @@ document.addEventListener("DOMContentLoaded", function () {
 				data: series1Keys.map(k => d[k])
 			}]
 		}
+
+		// set up the subcatchments checkboxes
+		const subcatchments = soefinding.findingJson.data.filter(c => c.Catchment == d.Catchment && c.Subcatchment != d.Subcatchment)
+		subcatchments.forEach((sc, i) => {
+			sc.checked = i == 0
+			sc.chartactive = true
+			sc.series = [{
+				name: sc.Subcatchment,
+				data: series1Keys.map(k => sc[k])
+			}]
+		})
+		soefinding.findingContent[d.Catchment].subcatchments = subcatchments
 	})
 
 	const options1 = soefinding.getDefaultLineChartOptions()
@@ -20,8 +32,10 @@ document.addEventListener("DOMContentLoaded", function () {
 	options1.xaxis.categories = series1Keys.map(k => k.replace("_", "–").split(" ")[0]) // ndash for year range
 	options1.xaxis.tickPlacement = "between"
 	options1.yaxis.title = "Percent (%)"
-	options1.yaxis.labels.formatter = val => val.toFixed(0)
+	options1.yaxis.labels.formatter = val => val.toFixed(1)
 	options1.tooltip.y = { formatter: val => `${val}%` } 
+	options1.yaxis.forceNicScale = false
+	options1.yaxis.min = 0
 
 
 	soefinding.state.chart1 = {
@@ -29,6 +43,9 @@ document.addEventListener("DOMContentLoaded", function () {
 		options: options1,
 		chartactive: true,
 	}
+
+	// set subcatchments for current region
+	soefinding.state.subcatchments = soefinding.findingContent[soefinding.state.currentRegionName].subcatchments
 
 
 	new Vue({
@@ -46,7 +63,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 	window.soefinding.onRegionChange = function () {
-		soefinding.state.chart1.series = this.findingContent[this.state.currentRegionName].series1
+		if (this.state.currentRegionName != "Queensland") {
+			soefinding.state.chart1.series = this.findingContent[this.state.currentRegionName].series1
+			soefinding.state.subcatchments = this.findingContent[this.state.currentRegionName].subcatchments
+		}
 	}
 
 })
