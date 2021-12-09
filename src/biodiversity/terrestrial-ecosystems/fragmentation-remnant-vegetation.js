6993b31720912db9a4bb991170525093aa6afd15
PORTER David <david.porter@daf.qld.gov.au>
2021-12-09 11:45:02 +1000

    add sort to series 2

--- src/biodiversity/terrestrial-ecosystems/fragmentation-remnant-vegetation.js
+++ src/biodiversity/terrestrial-ecosystems/fragmentation-remnant-vegetation.js
@@ -4,52 +4,56 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	const series1items = soefinding.findingJson.data.filter(d => d.Figure == 1)
 	const seriesFields = soefinding.findingJson.meta.fields.slice(-3)
-	const series1Labels = ["Patch value (%)", "Edge value (%)", "Core value (%)"] 
+	const series1Labels = ["Patch value (%)", "Edge value (%)", "Core value (%)"]
 	const years = new Set()
 	series1items.forEach(d => {
 		years.add(d.YEAR)
 
 		if (!soefinding.findingContent[d.REGPage])
-		soefinding.findingContent[d.REGPage] = {
-			series1: series1Labels.map(l =>  { return { name: l, data: []}})
-		}
+			soefinding.findingContent[d.REGPage] = {
+				series1: series1Labels.map(l => { return { name: l, data: [] } })
+			}
 
 		seriesFields.forEach((f, i) => soefinding.findingContent[d.REGPage].series1[i].data.push(d[f]))
 	})
 
 	const options1 = soefinding.getDefaultColumnChartOptions()
-	options1.tooltip.y = { formatter: val => val.toFixed(4) } 
-	options1.xaxis.categories = Array.from(years).map(y => [y.substring(0, y.lastIndexOf(" ")), y.substring(y.lastIndexOf(" ")+1)])
+	options1.tooltip.y = { formatter: val => val.toFixed(4) }
+	options1.xaxis.categories = Array.from(years).map(y => [y.substring(0, y.lastIndexOf(" ")), y.substring(y.lastIndexOf(" ") + 1)])
 	options1.xaxis.title.text = "Year"
 	options1.yaxis.labels.formatter = val => Math.round(val)
 	options1.yaxis.title.text = "Percent"
-	
+
 	soefinding.state.chart1 = {
 		options: options1,
 		series: soefinding.findingContent.Queensland.series1,
 		chartactive: true,
 	}
-	
+
 
 	// chart 2
-	const series2Labels = ["Patch value (count)", "Edge value (count)", "Core value (count)"] 
+	const series2Labels = ["Patch value (count)", "Edge value (count)", "Core value (count)"]
 	const series2items = soefinding.findingJson.data.filter(d => d.Figure == 2)
+	series2items.sort(function (a, b) {
+		return a.RegName >= b.RegName ? 1 : -1
+	})
 	const series2year = series2items[0].YEAR
 	series2items.forEach(d => {
-		if (! soefinding.findingContent[d.REGPage].series2) 
-			soefinding.findingContent[d.REGPage].series2 = series2Labels.map(l =>  { return { name: l, data: []}})
+		if (!soefinding.findingContent[d.REGPage].series2)
+			soefinding.findingContent[d.REGPage].series2 = series2Labels.map(l => { return { name: l, data: [] } })
 
 		seriesFields.forEach((f, i) => soefinding.findingContent[d.REGPage].series2[i].data.push(d[f]))
 
 		if (!soefinding.findingContent[d.REGPage].subregions)
 			soefinding.findingContent[d.REGPage].subregions = []
-		soefinding.findingContent[d.REGPage].subregions.push(d.RegName)	
+		soefinding.findingContent[d.REGPage].subregions.push(d.RegName)
 	})
 
 	const options2 = JSON.parse(JSON.stringify(options1))
+	options2.chart.height = 500
 	options2.chart.id = "chart2"
 	options2.tooltip.y.formatter = val => `${val < 0 ? '−' : ''}${Math.abs(val).toFixed(2)}`, // a better minus sign
-	options2.xaxis.categories = soefinding.findingContent[soefinding.state.currentRegionName].subregions
+		options2.xaxis.categories = soefinding.findingContent[soefinding.state.currentRegionName].subregions
 	options2.xaxis.labels.trim = soefinding.state.currentRegionName == "Brigalow Belt"
 	options2.xaxis.labels.hideOverlappingLabels = false
 	options2.xaxis.title.text = "Region"
@@ -58,8 +62,8 @@ document.addEventListener("DOMContentLoaded", function () {
 			return `${val < 0 ? '−' : ''}${Math.abs(val).toFixed(2)}`
 		else
 			return `${val < 0 ? '−' : ''}${Math.abs(val)}`
-	}, 
-	options2.yaxis.title.text = "Count per 100km²"
+	},
+		options2.yaxis.title.text = "Count per 100km²"
 
 	options2.xaxis.tickPlacement = "between" // not a good option, but it fixes a bug
 	// where the chart xaxis did not redraw correctly when swapping between regions.,
@@ -73,12 +77,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 	// chart 3
-	const series3Labels = ["Patch value (count per 100km²)", "Edge value (count per 100km²)", "Core value (count per 100km²)"] 
+	const series3Labels = ["Patch value (count per 100km²)", "Edge value (count per 100km²)", "Core value (count per 100km²)"]
 	const series3items = soefinding.findingJson.data.filter(d => d.Figure == 3)
 	const series3year = series3items[0].YEAR
 	series3items.forEach(d => {
-		if (! soefinding.findingContent[d.REGPage].series3) 
-			soefinding.findingContent[d.REGPage].series3 = series3Labels.map(l =>  { return { name: l, data: []}})
+		if (!soefinding.findingContent[d.REGPage].series3)
+			soefinding.findingContent[d.REGPage].series3 = series3Labels.map(l => { return { name: l, data: [] } })
 
 		seriesFields.forEach((f, i) => soefinding.findingContent[d.REGPage].series3[i].data.push(d[f]))
 
@@ -89,7 +93,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	const options3 = JSON.parse(JSON.stringify(options2))
 	options3.chart.id = "chart3"
 	options3.tooltip.y.formatter = val => `${val < 0 ? '−' : ''}${Math.abs(val)}`, // a better minus sign
-	options3.xaxis.categories = soefinding.findingContent[soefinding.state.currentRegionName].subregions
+		options3.xaxis.categories = soefinding.findingContent[soefinding.state.currentRegionName].subregions
 	options3.yaxis.labels.formatter = options1.yaxis.labels.formatter
 	options3.yaxis.title.text = "Count"
 
@@ -110,7 +114,7 @@ document.addEventListener("DOMContentLoaded", function () {
 		el: "#chartContainer",
 		data: soefinding.state,
 		computed: {
-			heading1: () => { 
+			heading1: () => {
 				let title = "Percent change in the number of fragmentation classes from previous reporting period"
 				if (soefinding.state.currentRegionName != "Queensland")
 					title += ` in ${soefinding.state.currentRegionName}`
@@ -159,8 +163,9 @@ document.addEventListener("DOMContentLoaded", function () {
 		ApexCharts.exec("chart2", "updateOptions", {
 			xaxis: {
 				categories: this.findingContent[this.state.currentRegionName].subregions,
-				labels: {trim: soefinding.state.currentRegionName == "Brigalow Belt"}
-			}})
+				labels: { trim: soefinding.state.currentRegionName == "Brigalow Belt" }
+			}
+		})
 		options2.xaxis.categories = this.findingContent[this.state.currentRegionName].subregions
 
 		//
@@ -172,8 +177,9 @@ document.addEventListener("DOMContentLoaded", function () {
 		ApexCharts.exec("chart3", "updateOptions", {
 			xaxis: {
 				categories: this.findingContent[this.state.currentRegionName].subregions,
-				labels: {trim: soefinding.state.currentRegionName == "Brigalow Belt"}
-			}})
+				labels: { trim: soefinding.state.currentRegionName == "Brigalow Belt" }
+			}
+		})
 		options3.xaxis.categories = this.findingContent[this.state.currentRegionName].subregions
 
 
