706ab21dcd1ae07b6c9902c9ba833290799ec333
PORTER David <david.porter@daf.qld.gov.au>
2021-12-13 13:11:14 +1000

    change to donut

--- src/biodiversity/estuarine-marine-ecosystems/marine-parks-fish.js
+++ src/biodiversity/estuarine-marine-ecosystems/marine-parks-fish.js
@@ -2,27 +2,27 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-	const series1Keys = ["Marine conservation park zone", "Marine national park zone", "Marine preservation zone", 
-		"Buffer zone", "Scientific research zone", "Fish habitat area management level A", 
+	const series1Keys = ["Marine conservation park zone", "Marine national park zone", "Marine preservation zone",
+		"Buffer zone", "Scientific research zone", "Fish habitat area management level A",
 		"Fish habitat area management level B"]
 
 	const series1KeyLabels = [
 		["Marine", "conservation", "park zone"],
-		["Marine national", "park zone"], 
-		["Marine", "preservation", "zone"], 
-		["Buffer zone"], 
-		["Scientific", "research zone"], 
-		["Fish habitat area", "management", "level A"], 
-		["Fish habitat area", "management", "level B"] 
+		["Marine national", "park zone"],
+		["Marine", "preservation", "zone"],
+		["Buffer zone"],
+		["Scientific", "research zone"],
+		["Fish habitat area", "management", "level A"],
+		["Fish habitat area", "management", "level B"]
 	] // there doesn't seem to be any way to get this done otherwise
 
-	const series1 = [{ 
+	const series1 = [{
 		name: "Hectares",
-	 	data: series1Keys.map(k => soefinding.findingJson.data[0][k])
-	 }]
+		data: series1Keys.map(k => soefinding.findingJson.data[0][k])
+	}]
 
 	const options1 = soefinding.getDefaultColumnChartOptions()
-	options1.tooltip.y = { formatter: val => val.toLocaleString() } 
+	options1.tooltip.y = { formatter: val => val.toLocaleString() }
 	options1.xaxis.categories = series1KeyLabels
 	options1.xaxis.title.text = "Protected area type"
 	options1.yaxis.title.text = "Hectares"
@@ -40,11 +40,16 @@ document.addEventListener("DOMContentLoaded", function () {
 	const series2 = series2Keys.map(k => soefinding.findingJson.data[0][k])
 
 	const options2 = soefinding.getDefaultPieChartOptions()
+	options2.chart.type = "donut"
 	options2.labels = series2Keys
-	options2.tooltip = { y: { formatter: (val, options) => {
-		const percent = options.globals.seriesPercent[options.seriesIndex][0]
-		return `${val.toLocaleString()}ha (${percent.toFixed(1)}%)`
-	}}}
+	options2.tooltip = {
+		y: {
+			formatter: (val, options) => {
+				const percent = options.globals.seriesPercent[options.seriesIndex][0]
+				return `${val.toLocaleString()}ha (${percent.toFixed(1)}%)`
+			}
+		}
+	}
 	options2.xaxis.categories = ["Protection", "Hectares"]
 
 	soefinding.state.chart2 = {
@@ -52,8 +57,6 @@ document.addEventListener("DOMContentLoaded", function () {
 		series: series2,
 		chartactive: true,
 	}
-
-
 
 
 	new Vue({
