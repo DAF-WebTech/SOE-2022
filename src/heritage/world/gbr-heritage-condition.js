43daac8c1c0634b2791df3887a9abeefadd9fef2
PORTER David <david.porter@daf.qld.gov.au>
2021-12-14 08:38:47 +1000

    add dial

--- src/heritage/world/gbr-heritage-condition.js
+++ src/heritage/world/gbr-heritage-condition.js
@@ -1,15 +1,26 @@
 "use strict"
 document.addEventListener("DOMContentLoaded", function () {
 
-	const series =  {
+	const series = {
 		tableHeadings: ["World Heritage natural criteria", "Condition summary", "Condition grade"],
-		data: soefinding.findingJson.data
+		data: soefinding.findingJson.data,
+		dial: {
+			number: 5,
+			val: "Good, Borderline Poor",
+			measure: "Condition",
+			rankings: ["Very Good", "Good", "Poor", "Very Poor"]
+		}
 	}
 
 
 	new Vue({
 		el: "#chartContainer",
-		data: series
+		data: series,
+		methods: {
+			getdialurl1: function () {
+				return soefinding.getDialUrl(this.dial.number)
+			}
+		}
 	})
 
 })
