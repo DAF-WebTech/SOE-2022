8575a6964d6cbcd37a5428c5c29a9d76ba612fa0
David Porter <david.porter@fastmail.com>
2021-12-15 13:39:22 +1000

    add "Others"

--- src/biodiversity/species-habitat/major-threats.js
+++ src/biodiversity/species-habitat/major-threats.js
@@ -2,25 +2,49 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-	const series = [
-		{
-			name: "Number of<br>threatened<br>species",
-			data: soefinding.findingJson.data.map(d => d[`Number threatened ${soefinding.biota}`])
-		}
-	]
 
-	const options = soefinding.getDefaultColumnChartOptions()
-	options.xaxis.categories = soefinding.findingJson.data.map(d => d.Threat.replace("-", "—"))
-	options.xaxis.labels.hideOverlappingLabels = false
+	const tableSeries = [{
+		name: "Number of<br>threatened<br>species",
+		data: soefinding.findingJson.data.map(d => d[`Number threatened ${soefinding.biota}`])
+	}]
+
+	const chartItems = soefinding.findingJson.data.slice(0, 30) //the first thirty
+	const otherItems = soefinding.findingJson.data.slice(30) // the last few
+	const chartSeries = [{
+		name: "Number of<br>threatened<br>species",
+		data: chartItems.map(d => d[`Number threatened ${soefinding.biota}`])
+	}]
+
+	chartSeries[0].data.push(
+		otherItems.reduce( function(acc, curr) { 
+			return acc + curr[`Number threatened ${soefinding.biota}`]}, 0)
+	)
+
+	const options = soefinding.getDefaultBarChartOptions()
+	options.chart.height = 500
+	options.xaxis.categories = chartItems.map(d => d.Threat.replace("-", "—"))
+	options.xaxis.categories.push("Others")
 	options.xaxis.labels.trim = true
+	options.plotOptions = {
+		bar: {
+			horizontal: true,
+		}
+	}
 	options.xaxis.title.text = "Threat"
+	options.yaxis.labels.minWidth = 300
+	options.yaxis.labels.maxWidth = 300
+	options.yaxis.title.offsetX = 15
 	options.yaxis.title.text = "Number of threatened species"
+
 
 	soefinding.state.chart1 = {
 		options,
-		series,
+		chartSeries,
+		tableSeries,
 		chartactive: true,
+		tableCategories: soefinding.findingJson.data.map(d => d.Threat.replace("-", "—"))
 	}
+
 
 
 	new Vue({
