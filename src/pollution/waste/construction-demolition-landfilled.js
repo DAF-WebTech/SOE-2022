5d89aa1d6bb6cd1dcc60f42a6e0f1b8e7acf72fe
PORTER David <david.porter@daf.qld.gov.au>
2021-12-20 15:11:54 +1000

    typo

--- src/pollution/waste/construction-demolition-landfilled.js
+++ src/pollution/waste/construction-demolition-landfilled.js
@@ -89,14 +89,14 @@ document.addEventListener("DOMContentLoaded", function () {
 		},
 		methods: {
 			formatter1: val => val?.toLocaleString() ?? "",
-		},
-		onStackedRadioClick: function () {
-			this.chart1.options.chart.type = "bar"
-			this.chart1.options.chart.stacked = true
-		},
-		onLineRadioClick: function () {
-			this.chart1.options.chart.type = "line"
-			this.chart1.options.chart.stacked = false
+			onStackedRadioClick: function () {
+				this.chart1.options.chart.type = "bar"
+				this.chart1.options.chart.stacked = true
+			},
+			onLineRadioClick: function () {
+				this.chart1.options.chart.type = "line"
+				this.chart1.options.chart.stacked = false
+			}
 		}
 	})
 })
