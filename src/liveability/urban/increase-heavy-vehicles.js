f98feaee93405449ef98bac752a5e043d13e6deb
PORTER David <david.porter@daf.qld.gov.au>
2022-01-11 13:55:32 +1000

    add line chart toggle

--- src/liveability/urban/increase-heavy-vehicles.js
+++ src/liveability/urban/increase-heavy-vehicles.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	const series = keys.map(k => {
 		return {
-			name: k, 
+			name: k,
 			data: soefinding.findingJson.data.map(d => d[k])
 		}
 	})
@@ -20,11 +20,11 @@ document.addEventListener("DOMContentLoaded", function () {
 	options.yaxis.labels.minWidth = 30
 	options.yaxis.labels.formatter = val => {
 		if (val >= 1000000)
-		  return `${Math.round(val / 1000000)}m`
+			return `${Math.round(val / 1000000)}m`
 		else
-          return `${Math.round(val / 1000)}k`
+			return `${Math.round(val / 1000)}k`
 	}
-	options.tooltip.y = { formatter: val => val.toLocaleString() } 
+	options.tooltip.y = { formatter: val => val?.toLocaleString() ?? "" }
 
 
 	soefinding.state.chart1 = {
@@ -41,7 +41,17 @@ document.addEventListener("DOMContentLoaded", function () {
 			heading1: () => `Number of light and heavy vehicles registered in Queensland`
 		},
 		methods: {
-			formatter1: val => val.toLocaleString()
+			formatter1: val => val.toLocaleString(),
+			onStackedRadioClick: function () {
+				this.chart1.options.chart.type = "bar"
+				this.chart1.options.chart.stacked = true
+			},
+			onLineRadioClick: function () {
+				this.chart1.options.chart.type = "line"
+				this.chart1.options.chart.stacked = false
+				this.chart1.options.markers = { size: 4 } // ignored by column chart
+				this.chart1.options.tooltip.shared = false
+			}
 		}
-	});
+	})
 })
