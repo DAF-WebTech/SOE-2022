58c1b2ce2d55b667541d6aad8084c0ee3ea52480
David Porter <david.porter@daf.qld.gov.au>
2022-01-11 15:09:26 +1000

    split xaxis names

--- src/liveability/urban/use-bikeways-seq.js
+++ src/liveability/urban/use-bikeways-seq.js
@@ -6,16 +6,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	const series = years.map(y => {
 		return {
-			name: y, 
+			name: y,
 			data: soefinding.findingJson.data.map(d => d[y])
 		}
 	})
 
+	const half = str => {
+		const commaPos = str.indexOf(",")
+		return [str.substring(0, commaPos + 1), str.substring(commaPos + 2)]
+	}
 
 	const options = soefinding.getDefaultColumnChartOptions()
-	options.xaxis.categories = soefinding.findingJson.data.map(d => d.Site)
+	options.xaxis.categories = soefinding.findingJson.data.map(d => half(d.Site))
 	options.xaxis.title.text = "Location"
-	options.xaxis.labels.trim = true,
+	options.xaxis.labels.trim = true
 	options.yaxis.title.text = "Average daily count"
 
 
@@ -30,11 +34,12 @@ document.addEventListener("DOMContentLoaded", function () {
 		el: "#chartContainer",
 		data: soefinding.state,
 		computed: {
-			heading1: () => `Daily use of bikeways ${years[0]} to ${years[years.length-1]}`
+			heading1: () => `Daily use of bikeways ${years[0]} to ${years[years.length - 1]}`
 		},
 		methods: {
 			formatter1: val => val?.toLocaleString() ?? ""
 
 		}
-	});
+	})
 })
+
