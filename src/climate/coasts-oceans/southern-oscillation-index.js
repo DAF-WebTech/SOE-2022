041084a5725c059e17de50308c759be31c8c0ffb
David Porter <david.porter@daf.qld.gov.au>
2022-01-17 09:32:54 +1000

    complete change for google chart

--- src/climate/coasts-oceans/southern-oscillation-index.js
+++ src/climate/coasts-oceans/southern-oscillation-index.js
@@ -1,59 +1,177 @@
+/* https://developers.google.com/chart/interactive/docs/gallery/controls?hl=en */
+
 "use strict"
-// to get the date series on y working properly, we had to 
-// use a different method of passing in data,
-// which affected the table render,
-// so we should make up a new component for this page,
-// on this page only.
-// to be done another time
-document.addEventListener("DOMContentLoaded", function () {
 
-	const items = soefinding.findingJson.data
 
-	const series = [{
-		name: "Six month mean",
-		data: items.map(d => {
-			return { x: `${d.Year}-${d.Month.toString().padStart(2, "0")}-01`, y: d["Six month mean"] }
-		})
-	}]
+// Load the Visualization API and the corechart package.
+google.charts.load("current", { packages:["corechart"]}).then(drawChart)
 
-	const options = soefinding.getDefaultLineChartOptions()
-	options.chart.animations = {
-		initialAnimation: {
-			enabled: false
+const lineChartDiv = document.querySelector('div.chart')
+
+function drawChart() {
+
+
+	const json = JSON.parse(document.getElementById("jsonData").textContent)
+	console.log(json)
+
+//soefinding.findingJson.data
+
+        // Create the data table.
+		const array = json.data.map(d => {
+				return [ new Date(d.Year, d.Month-1), d["Six month mean"] ] 
+			})
+		array.unshift([ {label: "Month", type: "date"}, "Six Month Mean"])
+
+
+        const data = google.visualization.arrayToDataTable(array)
+
+
+        // Set chart options
+        const options = {
+			chartArea: {
+				width: "90%",
+				height: "80%"
+			},
+			colors: ["#008FFB"],
+			explorer: {
+				actions: ["dragToZoom", "rightClickToReset"],
+				axis: "horizontal",
+				keepInBounds: true,
+				maxZoomIn: 0,
+			},
+			hAxis: {
+				textStyle: {
+					fontName: "Lato",
+					fontSize: 11
+				},
+				title: "Year",
+				titleTextStyle: { 	
+					bold: true,
+					italic: false,
+					fontSize: 13,
+					fontName: "Lato"
+				}
+			},
+			height: 450,
+			legend: {
+				position: "none"
+			},
+			lineWidth: 1,
+			tooltip: {
+				textStyle: {
+					fontName: "Lato",
+				}
+			},
+			vAxis: {
+				textStyle: {
+					fontName: "Lato",
+					fontSize: 11
+				},
+				title: "Index",
+				titleTextStyle: {
+					bold: true,
+					italic: false,
+					fontSize: 13,
+					fontName: "Lato"
+				}
+			},
+
+
 		}
-	}
-	options.tooltip.y = {
-		formatter: val => val < 0 ? `−${Math.abs(val).toFixed(1)}` : val.toFixed(1)
-	}
-	delete options.xaxis.categories
-	options.xaxis.title.text = "Year"
-	options.xaxis.type = "datetime"
-	options.yaxis.title.text = "Index"
-	options.yaxis.labels.formatter = val => val < 0 ? `−${Math.abs(val).toFixed(0)}` : val.toFixed(0)
-	options.stroke = {width: 1}
-	options.markers.size = 0
-	options.tooltip.x  = { format: 'MMMM yyyy' } 
 
-// taking the last 600 data points because they can't all fit
-// also need to fix the table
-series[0].data = series[0].data.slice(-600)
+        // Instantiate and draw our chart, passing in some options.
+        const chart = new google.visualization.LineChart(lineChartDiv);
+        chart.draw(data, options);
 
 
-	soefinding.state.chart1 = {
-		options: options,
-		series: series,
-		chartactive: true,
-	};
 
 
-	new Vue({
-		el: "#chartContainer",
-		data: soefinding.state,
-		computed: {
-			heading1: function () { return `Southern Oscillation Index 1876–2020` },
-		},
-		methods: {
-			formatter1: val => val?.toFixed(2) ?? ""
-		},
-	});
+		const tableBody = document.getElementById("tableBody")
+		const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+		array.forEach( (a, i) => {
+			if (i > 0)
+				tableBody.insertAdjacentHTML("beforeend", 
+					`<tr><th scope=row>${months[a[0].getMonth()]} ${a[0].getFullYear()}<td class=num>${(a[1]?.toFixed(2) ?? "")
+					.replace(/\-/, "−")}`
+					)
+		})
+
+
+
+
+
+}
+
+
+
+// table/chart toggle
+const chartListItems = document.querySelectorAll("ul.chart-tabs li")
+const chartListItem = chartListItems[0]
+const tableListItem = chartListItems[1]
+
+const divs = document.querySelectorAll("div.chart-table>div")
+const chartDiv = divs[0]
+const tableDiv = divs[1]
+
+document.querySelector("ul.chart-tabs").addEventListener("click", function(event) {
+		if (event.target.parentElement == chartListItem) {
+			chartListItem.classList.add("active")
+			tableListItem.classList.remove("active")
+
+			chartDiv.classList.remove("inactive")
+			tableDiv.classList.add("inactive")
+
+		}
+		else if (event.target.parentElement == tableListItem) {
+			chartListItem.classList.remove("active")
+			tableListItem.classList.add("active")
+
+			chartDiv.classList.add("inactive")
+			tableDiv.classList.remove("inactive")
+		}
+	
+
 })
+
+
+// chart interactive buttons
+document.getElementById("resetButton").addEventListener("click", function() { 
+
+	const evt = new MouseEvent("mousedown", {
+		view: window,
+		button: 2,
+		bubbles: true,
+		cancelable: true
+
+	})
+	lineChartDiv.firstElementChild.dispatchEvent(evt)
+	lineChartDiv.firstElementChild.firstElementChild.dispatchEvent(evt)
+	lineChartDiv.firstElementChild.firstElementChild.firstElementChild.dispatchEvent(evt)
+	
+
+	const svg = lineChartDiv.firstElementChild.firstElementChild.firstElementChild.firstElementChild
+	svg.dispatchEvent(evt)
+
+
+})
+
+const zoomButton = document.getElementById("resetButton")
+zoomButton.addEventListener("click", function() { 
+
+	zoomButton.classList.add("active")
+	panButton.classList.remove("active")
+
+
+
+})
+
+const panButton = document.getElementById("panButton")
+panButton.addEventListener("click", function() { 
+
+	zoomButton.classList.remove("active")
+	panButton.classList.add("active")
+
+
+})
+
+
