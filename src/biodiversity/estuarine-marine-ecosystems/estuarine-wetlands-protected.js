8e798f53a8adbac0ef6a479b22c5ed2c065dd39d
David Porter <david.porter@daf.qld.gov.au>
2022-01-17 11:41:15 +1000

    use percent table on pie chart

--- src/biodiversity/estuarine-marine-ecosystems/estuarine-wetlands-protected.js
+++ src/biodiversity/estuarine-marine-ecosystems/estuarine-wetlands-protected.js
@@ -63,6 +63,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	soefinding.state.chart2 = {
 		options: options2,
 		series: series2,
+		seriesSum: series2.reduce(function (acc, curr) { return acc + curr}, 0),
 		chartactive: true,
 	}
 
@@ -78,6 +79,9 @@ document.addEventListener("DOMContentLoaded", function () {
 		},
 		methods: {
 			formatter1: val => val.toLocaleString(),
+			formatPercent: function(s) { 
+				return (s / this.chart2.seriesSum * 100 ).toFixed(1)
+			}
 		}
 	})
 })
