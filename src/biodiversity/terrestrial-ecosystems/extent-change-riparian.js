7c57a8ca062d42a48bec37a1be68d75ebea9bd9b
David Porter <david.porter@daf.qld.gov.au>
2022-01-18 15:44:14 +1000

    call load finding html

--- src/biodiversity/terrestrial-ecosystems/extent-change-riparian.js
+++ src/biodiversity/terrestrial-ecosystems/extent-change-riparian.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	const catchments = soefinding.findingJson.data.filter(d => d.Catchment == d.Subcatchment)
 	const series1Keys = soefinding.findingJson.meta.fields.filter(f => f.includes("Woody Vegetation Loss per cent"))
-	soefinding.findingContent.Queensland = { series1: null, subcatchments: null}
+	soefinding.findingContent.Queensland = { series1: null, subcatchments: null }
 
 	catchments.forEach(d => {
-		soefinding.findingContent[d.Catchment] = { 
+		soefinding.findingContent[d.Catchment] = {
 			series1: [{
 				name: d.Catchment,
 				data: series1Keys.map(k => d[k])
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	options1.xaxis.tickPlacement = "between"
 	options1.yaxis.title = "Percent (%)"
 	options1.yaxis.labels.formatter = val => val.toFixed(1)
-	options1.tooltip.y = { formatter: val => `${val}%` } 
+	options1.tooltip.y = { formatter: val => `${val}%` }
 	options1.yaxis.forceNicScale = false
 	options1.yaxis.min = 0
 
@@ -52,7 +52,7 @@ document.addEventListener("DOMContentLoaded", function () {
 		el: "#chartContainer",
 		data: soefinding.state,
 		computed: {
-			heading1: function()  { return `Loss of riparian woody vegetation in ${this.currentRegionName}` },
+			heading1: function () { return `Loss of riparian woody vegetation in ${this.currentRegionName}` },
 		},
 		methods: {
 			formatter1: val => val.toLocaleString()
@@ -67,6 +67,8 @@ document.addEventListener("DOMContentLoaded", function () {
 			soefinding.state.chart1.series = this.findingContent[this.state.currentRegionName].series1
 			soefinding.state.subcatchments = this.findingContent[this.state.currentRegionName].subcatchments
 		}
+
+		soefinding.loadFindingHtml()
 	}
 
 })
