b8c36f7771244196de307edc15bcab694cd776f2
David Porter <david.porter@daf.qld.gov.au>
2022-03-11 09:15:08 +1000

    fix reactivity

--- src/pollution/water-quality/condition-aquatic-ecosystem.js
+++ src/pollution/water-quality/condition-aquatic-ecosystem.js
@@ -3,10 +3,4 @@ document.addEventListener("DOMContentLoaded", function () {
 	soefinding.regionNames.forEach(r => soefinding.findingContent[r] = {})
 
 
-
-
-	window.soefinding.onRegionChange = function () {
-		soefinding.loadFindingHtml()
-	}
-
 })
